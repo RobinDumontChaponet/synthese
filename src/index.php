189b9637d43f0df7bd069d972eac78d26245454c
Robin Dumont-Chaponet <r-dc@r-dcsrMBP.local>
2014-11-15 22:30:28 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -40,35 +40,17 @@ if($matches[1]) {
 	}
 	if($onload!='') $script.="\n".'<script type="text/javascript">window.onload=function(){'.$onload.'}</script>';
 }
-
-/*$start=strpos($inc, '<!--meta') + strlen('<!--meta');
-$tag=trim(substr($inc,$start,strpos($inc, '-->')-$start));
-if($tag) {
-	$link=''; $script=''; $onload='';
-	$tag=preg_split('/\s+/',$tag);
-	foreach($tag as $rule) {
-		$param=substr($rule,0,strpos($rule,'"'));
-		$arg=substr($rule,strpos($rule,'"')+1,strrpos($rule,'"')-strpos($rule,'"')-1);
-		switch($param){
-			case"title=": $title=$arg; break;
-			case"css=": $link.='<link rel="stylesheet" type="text/css" href="'.$arg.'"/>'."\n"; break;
-			case"js=":  $script.='<script type="text/javascript" src="'.$arg.'"></script>'."\n"; break;
-			case"onload=":  $onload.=$arg.'();'; break;
-		}
-	}
-	if($onload!='') $script.="\n".'<script type="text/javascript">window.onload=function(){'.$onload.'}</script>';
-}*/
 ?>
 <html>
   <head>
     <title><?php echo $title; ?></title>
 	<meta charset="UTF-8">
-	  <link rel="stylesheet" type="text/css" href="style/reset.min.css">
-	  <link rel="stylesheet" type="text/css" href="style/general.css">
-	  <?php echo $link; ?>
-	  <script type="text/javascript" src="script/polyShims.js"></script>
-	  <script type="text/javascript" src="script/transit.js"></script>
-	  <?php echo $script; ?>
+	<link rel="stylesheet" type="text/css" href="style/reset.min.css">
+	<link rel="stylesheet" type="text/css" href="style/general.css">
+	<?php echo $link; ?>
+	<script type="text/javascript" src="script/polyShims.js"></script>
+	<script type="text/javascript" src="script/transit.js"></script>
+	<?php echo $script; ?>
   </head>
   <body>
   	<?php echo $inc; ?>
