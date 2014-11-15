073fcb863c60a1fbdfa60ffec781d2bc7723ee89
lmaginot <mathieu@lalignemaginot.fr>
2014-11-15 10:55:47 +0100

    modifié :         src/index.php 	src/connexion.php 	src/styles.php 	src/vues/

--- src/index.php
+++ src/index.php
@@ -1,4 +1,13 @@
- function get_include_contents($filename) {
+<?php
+    session_start();
+    require_once("connexion.php");
+    require_once("styles.php");
+    if(!isset($_SESSION['synt_user'])){
+        header('Location: login');
+    }else if(!isset($_GET['page'])){
+        header('Location: accueil');   
+    }
+    function get_include_contents($filename) {
 		if (is_file('pages/'.$filename)) {
 			ob_start();
 			include $filename;
@@ -6,7 +15,7 @@
 		}
 		return false;
 	}
-	$inc = get_include_contents(preg_quote(strip_tags($_GET['r'])).'.php');
+	$inc = get_include_contents(preg_quote(strip_tags($_GET['page'])).'.php');
 	$start=strpos($inc, '<!--meta') + strlen('<!--meta');
 	$tag=trim(substr($inc,$start,strpos($inc, '-->')-$start));
 	if($tag) {
@@ -24,3 +33,28 @@
 		}
 		if($onload!='') $script.="\n".'<script type="text/javascript">window.onload=function(){'.$onload.'}</script>';
 	}
+?>
+    <html>
+        <head>
+            <title><?= $title; ?></title>
+            <meta charset="UTF-8">
+            <link rel="stylesheet" type="text/css" href="style/reset.min.css">
+            <link rel="stylesheet" type="text/css" href="style/general.css">
+            <?php echo $link; ?>
+        </head>
+        
+        <body>
+            <?php
+                if(isset($_GET['page'])){
+                    if(file_exists('controls/'.$_GET['page'].'.php')){
+                        require_once('controls/'.$_GET['page'].'.php');
+                    }
+                    if(file_exists('vues/'.$_GET['page'].'.php')){
+                       echo $inc;
+                    }else{
+                        require_once('vues/404.php');   
+                    }
+                }
+            ?>
+        </body>
+    </html>
