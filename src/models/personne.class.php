cb7c465cdd90e27c01a01be1a6c46c91f76023f1
fadiliyoussef <fadiliYouss@gmail.com>
2014-11-16 09:44:56 +0100

    Update personne.class.php

--- src/models/personne.class.php
+++ src/models/personne.class.php
@@ -1,4 +1,44 @@
-	<?php public function setIdAncien($idAncien)
+<?php
+	
+class Personne
+{
+
+	private $id;
+	private $nom;
+	private $nomPatronymique;
+	private $prenom;
+	
+	public Personne($id, $nom, $nomPatronymique, $prenom)
+	{
+		$this->id = $id;
+		$this->nom = $nom;
+		$this->nomPatrnoymique = $nomPatronymique;
+		$this->prenom = $prenom;
+	}
+
+//----------------------------------Getters
+	public function getId()
+	{
+		return $this->id;
+	}
+	
+	public function getNom()
+	{
+		return $this->nom;
+	}
+	
+	public function getNomPatroymique()
+	{
+		return $this->nomPatronymique;
+	}
+	
+	public function getPrenom()
+	{
+		return $this->prenom;
+	}
+
+//----------------------------------Setters
+	public function setId($idAncien)
 	{
 		if(($idAncien != null) and ($idAncien > 0))
 		{
@@ -8,4 +48,49 @@
 			throw new Exception("Id ancien invalide");
 		}
 	}
-	?>
+	
+	public function setNom($nom)
+	{
+		$nomTraite = trim($nom);
+		if(($nomTraite != null) and ($nomTraite != ""))
+		{
+			$this->nom = strtoupper($nomTraite);
+		}else
+		{
+			throw new Exception("Nom incorrect");
+		}
+	}
+	
+	public function setNomPatronymique($nomPatronymique)
+	{
+		$nomPatronymiqueTraite = trim($nomPatronymique);
+		if(($nomPatronymiqueTraite != null) and ($nomPatronymiqueTraite != ""))
+		{
+			$this->nomPatronymique = strtoupper($nomPatronymiqueTraite);
+		}else
+		{
+			throw new Exception("Nom incorrect");
+		}	
+	}
+	
+	public function setPrenom($prenom)
+	{
+		$prenomTraite = trim($prenom);
+		if(($prenomTraite != null) and ($prenomTraite != ""))
+		{
+			$this->prenom = strtoupper(substr($prenomTraite,0,1)).strtolower(substr($prenomTraite,1));
+		}else
+		{
+			throw new Exception("Prenom incorrect");
+		}
+	}
+	
+//------------------------------tostring
+	public function toString()
+	{
+		return "Id : ".$this->id." Nom : ".$this->nom." Nom patronymique : "
+			.$this->nomPatrnoymique." Prénom : ".$this->prenom;
+	}
+}
+	
+?>
