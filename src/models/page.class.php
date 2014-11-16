8d312809388cbd515252eba73ee7be179501950d
fadiliyoussef <fadiliYouss@gmail.com>
2014-11-16 14:20:54 +0100

    Create page.class.php

---
+++ src/models/page.class.php
@@ -0,0 +1,58 @@
+<?php
+
+class Page
+{
+
+  private $id;
+  private $libelle;
+
+  public function Page($id, $libelle)
+  {
+      $this->setId($id);
+      $this->setLibelle($libelle);
+  }
+  
+//--------------------------------------------Getters
+  public function getId()
+  {
+      return $this->id;
+  }
+  
+  public function getLibelle()
+  {
+      return $this->libelle;
+  }
+
+//--------------------------------------------Setters
+
+  public function setId($id)
+  {
+    if(($id != null) and ($id != ""))
+    {
+        $this->id = $id;
+    }else
+    {
+        throw new Exception("Id page incorrect");
+    }
+  }
+
+public function setLibelle($libelle)
+{
+  $libelleTraite = trim($libelle);
+  if(($libelleTraite != null) and ($libelleTraite != ""))
+  {
+      $this->libelle = $libelleTraite;
+  }else
+  {
+      throw new Exception("Libellé page incorrect");
+  }
+}
+//-------------------------------------------toString
+  public function toString()
+  {
+    return "Id : ".$this->id." Libellé : ".$this->libelle;
+  }
+
+}
+
+?>
