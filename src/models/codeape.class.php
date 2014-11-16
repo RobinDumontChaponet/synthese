e6b7da24429ac8993598c359fa4e8f2d90097de0
fadiliyoussef <fadiliYouss@gmail.com>
2014-11-16 16:14:31 +0100

    Create codeape.class.php

---
+++ src/models/codeape.class.php
@@ -0,0 +1,56 @@
+<?php
+
+class CodeAPE
+{
+
+  private $code;
+  private $libelle;
+
+  public function __construct($id, $libelle)
+  {
+      $this->setCode($code);
+      $this->setLibelle($libelle);
+  }
+  
+//--------------------------------------------Getters
+  public function getCode()
+  {
+      return $this->code;
+  }
+  
+  public function getLibelle()
+  {
+      return $this->libelle;
+  }
+//--------------------------------------------Setters
+  public function setId($code)
+  {
+    if(($id != null))
+    {
+        $this->id = $id;
+    }else
+    {
+        throw new Exception("Code ape incorrect");
+    }
+  }
+  public function setLibelle($libelle)
+  {
+    $libelleTraite = trim($libelle);
+    if(($libelleTraite != null) and ($libelleTraite != ""))
+    {
+        $this->libelle = $libelleTraite;
+    }else
+    {
+        throw new Exception("Libellé diplome incorrect");
+    }
+  }
+//-------------------------------------------toString
+  public function __toString()
+  {
+    return "Code : ".$this->code." Libellé : ".$this->libelle;
+  }
+}
+
+}
+
+?>
