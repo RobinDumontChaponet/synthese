7d1a9daed9f732d8f0d172167396791a71f750bd
fadiliyoussef <fadiliYouss@gmail.com>
2014-11-16 16:20:24 +0100

    Create specialisation.class.php

---
+++ src/models/specialisation.class.php
@@ -0,0 +1,51 @@
+<?php
+class Specialisation
+{
+  private $id;
+  private $libelle;
+  
+  public function __construct($id, $libelle)
+  {
+      $this->setId($id);
+      $this->setLibelle($libelle);
+  }
+  
+//--------------------------------------------Getters
+  public function getId()
+  {
+      return $this->id;
+  }
+  
+  public function getLibelle()
+  {
+      return $this->libelle;
+  }
+//--------------------------------------------Setters
+  public function setId($id)
+  {
+    if(($id != null) and ($id > 0))
+    {
+        $this->id = $id;
+    }else
+    {
+        throw new Exception("Id spécialisation incorrect");
+    }
+  }
+  public function setLibelle($libelle)
+  {
+    $libelleTraite = trim($libelle);
+    if(($libelleTraite != null) and ($libelleTraite != ""))
+    {
+        $this->libelle = $libelleTraite;
+    }else
+    {
+        throw new Exception("Libellé spécialisation incorrect");
+    }
+  }
+//-------------------------------------------toString
+  public function __toString()
+  {
+    return "Id : ".$this->id." Libellé : ".$this->libelle;
+  }
+}
+?>
