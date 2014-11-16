915d8b79e0a37a6f6da8ac0c54afb1984b44629c
fadiliyoussef <fadiliYouss@gmail.com>
2014-11-16 09:56:49 +0100

    Update promotion.class.php

--- src/models/promotion.class.php
+++ src/models/promotion.class.php
@@ -1,7 +1,52 @@
 <?php
 
-public function promotion()
-{
-}
+  
+  private $id;
+  private $annee;
+  
+  public function promotion($id, $annee)
+  {
+      $this->setId($id);
+      $this->setAnnee($annee);
+  }
+  
+//----------------------------Getters
+  public function getId()
+  {
+    return $this->id;
+  }
+  
+  public function getAnnee()
+  {
+    return $this->annee;
+  }
+  
+//---------------------------Setters
+  public function($id)
+  {
+      if(($id != null) and ($id > 0))
+      {
+          $this->id = $id;
+      }
+      else
+      {
+          throw new Exception("Id incorrect");
+      }
+  }
+  
+  public function($annee)
+  {
+      if(preg_match("/^[0-9][0-9][0-9][0-9]$/", $annee))
+      {
+          $this->annee
+      }
+  }
+  
+  
+//-----------------------------toString
+  public function toString()
+  {
+      return "Id : ".$this->id." Annee : ".$this->annee;
+  }
 
 ?>
