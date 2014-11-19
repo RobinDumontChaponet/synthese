edab6b4069f13b7c17b4f8d3668e15d93bcce76a
RobinDumontChaponet <robin@dumontchapo.net>
2014-11-19 18:44:23 +0100

    correct

--- src/includes/dbConnection.inc.php
+++ src/includes/dbConnection.inc.php
@@ -1,13 +1,11 @@
 <?php
-
 function connect () {
 	try {
-		$connection = new PDO( 'mysql:host=localhost;dbname=projetSynthese', 'jozwicki2u_appli', 'veajoighapboagityet', array(
+		$connection = new PDO('mysql:host=infodb2.iut.univ-metz.fr;dbname=jozwicki2u_projetSynthese', 'jozwicki2u_appli', 'veajoighapboagityet', array(
 		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 		return $connection;
 	} catch ( Exception $e ) {
 		echo "Connection à MySQL impossible : ", $e->getMessage();
 	}
 }
-
 ?>
