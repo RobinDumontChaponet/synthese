c8cfff45806e719ef405e392ff8b7d395e90404c
RobinDumontChaponet <robin@dumontchapo.net>
2014-11-19 19:10:17 +0100

    correct encore...

--- src/includes/dbConnection.inc.php
+++ src/includes/dbConnection.inc.php
@@ -1,7 +1,7 @@
 <?php
 function connect () {
 	try {
-		$connection = new PDO('mysql:host=infodb2.iut.univ-metz.fr;dbname=synthese', 'root', 'veajoighapboagityet', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$connection = new PDO('mysql:host=infodb2.iut.univ-metz.fr;dbname=jozwicki2u_projetSynthese', 'jozwicki2u_appli', 'veajoighapboagityet', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 		return $connection;
 	} catch ( Exception $e ) {
 		echo "Connection à MySQL impossible : ", $e->getMessage();
