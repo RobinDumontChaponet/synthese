09a77357c485ee85fcc38b474bcd4fa72e9b993b
fadiliyoussef <fadiliYouss@gmail.com>
2014-11-19 12:37:54 +0100

    Update Personne.class.php

--- src/models/Personne.class.php
+++ src/models/Personne.class.php
@@ -38,11 +38,11 @@ class Personne
 	}
 
 //----------------------------------Setters
-	public function setId($idAncien)
+	public function setId($id)
 	{
-		if(($idAncien != null) and ($idAncien > 0))
+		if(($id != null) and ($id > 0))
 		{
-			$this->idAncien = $idAncien;
+			$this->id = $id;
 		}else
 		{
 			throw new Exception("Id personne invalide");
