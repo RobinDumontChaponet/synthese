5e7dc60914a1ff576eba15897234f460a6098806
lmaginot <mathieu@lalignemaginot.fr>
2014-11-19 19:23:24 +0100

    Signed-off-by: lmaginot <mathieu@lalignemaginot.fr>

--- src/models/CodeAPEDAO.class.php
+++ src/models/CodeAPEDAO.class.php
@@ -1,52 +1,52 @@
 <?php
     require_once("includes/dbConnection.inc.php");
-    require_once("models/Promotion.class.php");
+    require_once("models/CodeAPE.class.php");
 
-    class PromotionDAO{
+    class CodeAPEDAO{
         
         public static function getAll(){
             $bdd=$connect();
-            $req=$bdd->query("SELECT `idPromo`, `annee` FROM `promotion` ORDER BY annee");
+            $req=$bdd->query("SELECT `code`, `libelle` FROM `codeAPE` ORDER BY code");
             $lst=$req->fetchAll();
-            $lstPromo=array();
-            foreach($lst as $promo){
-                $lstPromo[]=new Promotion($promo['idPromo'], $promo['annee']);   
+            $lstcode=array();
+            foreach($lst as $code){
+                $lstcode[]=new CodeAPE($code['code'], $code['libelle']);   
             }
             return $lstPromo;
         }
         
-        public static function getById($id){
-            if(is_numeric($id)){
+        public static function getById($code){
+            
                 $bdd=connect();
-                $req=$bdd->prepare("SELECT `idPromo`, `annee` FROM `promotion` WHERE idPromo=?");
+                $req=$bdd->prepare("SELECT `code`, `libelle` FROM `codeAPE` WHERE code=?");
                 $req->execute(array($id));
-                $promo=$req->fetch();
-                return new Promotion($promo['idPromo'], $promo['annee']);
-            }
+                $code=$req->fetch();
+                return new CodeAPE($code['code'], $code['libelle']);
+            
         }
         
-        public static function create($promo){
-            if(gettype($promo)=="Promotion"){
+        public static function create($code){
+            if(gettype($promo)=="CodeAPE"){
                 $bdd->connect();
-                $req=$bdd->prepare("INSERT INTO `promotion`(`annee`) VALUES (?)");
-                $req->execute(array($promo->getAnnee()));
+                $req=$bdd->prepare("INSERT INTO `codeAPE`(`code`, `libelle`) VALUES (?,?)");
+                $req->execute(array($code->getCode(),$code->getLibelle()));
                 return $bdd->LastInsertId();
             }
         }
         
         public static function update($promo){
-            if(gettype($promo)=="Promotion"){
+            if(gettype($promo)=="CodeAPE"){
                 $bdd->connect();
-                $req=$bdd->prepare("UPDATE `promotion` SET `annee`=? WHERE idPromo=?");
-                $req->execute(array($promo->getAnnee(), $promo->getId()));
+                $req=$bdd->prepare("UPDATE `codeAPE` SET `libelle`=? WHERE code=?");
+                $req->execute(array($code->getLibelle(),$code->getCode()));
             }
         }
         
-        public static function delete($promo){
-             if(gettype($promo)=="Promotion"){
+        public static function delete($code){
+             if(gettype($promo)=="CodeAPE"){
                 $bdd->connect();
-                $req=$bdd->prepare("DELETE FROM `promotion` WHERE `idPromo`=?");
-                 $req->execute(array($promo->getId()));
+                $req=$bdd->prepare("DELETE FROM `codeAPE` WHERE `code`=?");
+                 $req->execute(array($code->getCode()));
              }
         }
         
