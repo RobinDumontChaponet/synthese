6257aded3f3b378de9f81537b86287e32d67b8b9
lmaginot <mathieu@lalignemaginot.fr>
2014-11-19 14:21:42 +0100

    Signed-off-by: lmaginot <mathieu@lalignemaginot.fr>

--- src/includes/dbConnection.inc.php
+++ src/includes/dbConnection.inc.php
@@ -1,8 +1,8 @@
 <?php
 
-function connect ($dns) {
+function connect () {
 	try {
-		$connection = new PDO( $dns, 'jozwicki2u_appli', 'veajoighapboagityet', array(
+		$connection = new PDO( 'mysql:host=infodb2.iut.univ-metz.fr;dbname=jozwicki2u_projetSynthese', 'jozwicki2u_appli', 'veajoighapboagityet', array(
 		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 		return $connection;
 	} catch ( Exception $e ) {
---
+++ src/models/AncienDAO.class.php
@@ -0,0 +1,12 @@
+<?php
+    require_once("includes/dbConnection.inc.php");
+
+    abstract class AncienDAO{
+        
+        
+        public abstract function getAll(){
+            // appelle de la connexion
+        }
+        
+    }
+?>
