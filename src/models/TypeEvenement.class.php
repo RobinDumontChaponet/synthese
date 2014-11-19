40a25c2fc98921f759d5bff3e67fc2e613065335
fadiliyoussef <fadiliYouss@gmail.com>
2014-11-19 12:39:55 +0100

    Create TypeEvenement.class.php

---
+++ src/models/TypeEvenement.class.php
@@ -0,0 +1,59 @@
+<?php
+
+class TypeEvenement
+{
+
+  private $id;
+  private $libelle;
+  
+  public function __construct($id, $libelle)
+  {
+    $this->setId($id);
+    $this->setLibelle($libelle);
+  }
+  
+//------------------------------------------GETTERS------------------------------------
+  public function getId()
+  {
+    return $this->id;
+  }
+  
+  public function getLibelle()
+  {
+    return $this->libelle;
+  }
+  
+//-------------------------------------SETTERS---------------------------------------
+  public function setId($id)
+	{
+		if(($id != null) and ($id > 0))
+		{
+			$this->id = $id;
+		}else
+		{
+			throw new Exception("Id type evenement invalide");
+		}
+	}
+	
+	public function setLibelle($libelle)
+  {
+    $libelleTraite = trim($libelle);
+    if(($libelleTraite != null) and ($libelleTraite != ""))
+    {
+        $this->libelle = $libelleTraite;
+    }else
+    {
+        throw new Exception("Libellé type evenement incorrect");
+    }
+  }
+	
+	
+//------------------------------------toString-------------------------------------
+  public function __toString()
+  {
+    return "Id : ".$this->id." Libelle : ".$this->libelle;
+  }
+
+}
+
+?>
