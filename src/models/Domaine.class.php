41b288891211c3b90b9c808f287fc34d6dd1f24a
fadiliyoussef <fadiliYouss@gmail.com>
2014-12-08 20:28:25 +0100

    Create Domaine.class.php

---
+++ src/models/Domaine.class.php
@@ -0,0 +1,86 @@
+<?php
+
+class Domaine
+{
+    
+    private $id;
+    private $libelle;
+    private $description;
+    
+    public function __construct($id, $libelle, $description)
+    {
+        $this->setId($id);
+        $this->setLibelle($libelle);
+        $this->setDescription($description);
+    }
+    
+    //-------------------------------GETTERS--------------------------------
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+    
+    public function getDescription()
+    {
+        return $this->description;
+    }
+    
+    //--------------------------------SETTERS-------------------------------
+    public function setId($id)
+	{
+		if(($id != null) and ($id >= 0))
+		{
+			$this->id = $id;
+		}else
+		{
+			throw new Exception("Id personne invalide");
+		}
+	}
+    
+    public function setLibelle($libelle)
+	{
+		$libelleTraite = trim($libelle);
+		if(($libelleTraite != null) and ($libelleTraite != ""))
+		{
+			$this->libelle = $libelleTraite;
+		}else
+		{
+			throw new Exception("Libelle incorrect");
+		}
+	}
+    
+    public function setDescription($description)
+	{
+		$descriptionTraite = trim($description);
+		if(($descriptionTraite != null))
+		{
+			if($descriptionTraite != "")
+            {
+                $this->description = $libelleTraite;
+            }
+            else
+            {
+                $this->description = "Aucune description !";
+            }
+		}else
+		{
+			throw new Exception("Description incorrecte");
+		}
+	}
+    
+    //-------------------------------toString--------------------------
+    
+    public function __toString()
+    {
+        return "Id : ".$this->id." Libelle : ".$this->libelle." Description : ".$this->desciption;
+    }
+    
+}
+
+
+?>
