5a646c29eb5b06c011e833f21bf5fce789e7f83f
fadiliyoussef <fadiliYouss@gmail.com>
2014-12-08 20:43:10 +0100

    Create Possede.class.php

---
+++ src/models/Possede.class.php
@@ -0,0 +1,112 @@
+<?php
+
+class Possede
+{
+    private $ancien;
+    private $etablissement;
+    private $diplomePostDUT;
+    private $resultat;
+    private $dateDebut;
+    private $dateFin;
+    
+    public function __construct($ancien, $etablissement, $diplomePostDUT, $resultat, $dateDebut, $dateFin)
+    {
+        $this->setAncien($ancien);
+        $this->setEtablissement($etablissement);
+        $this->setDiplomePostDUT($diplomePostDUT);
+        $this->setResultat($resultat);
+        $this->setDateDebut($dateDebut);
+        $this->setDateFin($dateFin);
+    }
+    
+    //-------------------------------------GETTERS-----------------------
+    public function getAncien()
+    {
+        return $this->ancien;
+    }
+    
+    public function getEtablissement()
+    {
+        return $this->etablissement;
+    }
+    
+    public function getDiplomePostDUT()
+    {
+        return $this->diplomePostDUT;
+    }
+    
+    public function getResultat()
+    {
+        return $this->resultat;
+    }
+    
+    public function getDateDebut()
+    {
+        return $this->dateDebut;
+    }
+    
+    public function getDateFin()
+    {
+        return $this->dateFin;
+    }
+    
+    //-------------------------------SETTERS------------------------------------
+    public function setAncien($ancien)
+    {
+        if($ancien != null)
+        {
+            $this->ancien = $ancien;
+        }else{
+            throw new Exception("Ancien dans Possede est nul !");
+        }
+    }
+
+    public function setEtablissement($etablissement)
+    {
+        if($etablissement != null)
+        {
+            $this->etablissement = $etablissement
+        }else{
+            throw new Exception("Etablissement dans Possede est nul !")
+        }
+    }
+    
+    public function setDiplomePostDUT($diplomePostDUT)
+    {
+        if($diplomePostDUT != null)
+        {
+            $this->diplomePostDUT = $diplomePostDUT;
+        }else{
+            throw new Exception("Diplome post DUT dans Possede est nul !");
+        }
+    }
+    
+    public function setDateDebut($dateDebut)
+    {
+        if($dateDebut != null)
+        {
+            $this->dateDebut = $dateDebut;
+        }else{
+            throw new Exception("Date début dans Possede est nulle !");
+        }
+    }
+    
+    public function setDateFin($dateFin)
+    {
+        if($dateFin != null)
+        {
+            $this->dateFin = $dateFin;
+        }else{
+            throw new Exception("Date fin dans Possede est nulle !");
+        }
+    }
+    
+    //----------------------------------------------toString---------------------------------------
+    public function __toString()
+    {
+        return "Ancien : ".$this->ancien->__toString()." Etablissement : ".$this->etablissement->__toString()." Diplome post DUT : ".$this->diplomePostDUT->__toString()." Date debut : ".$this->dateDebut." Date fin : ".$this->dateFin;
+    }
+    
+}
+
+?>
