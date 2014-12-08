0fc7275486a9df752eebbc5001f9cc1a8ebc99dd
fadiliyoussef <fadiliYouss@gmail.com>
2014-12-08 21:32:31 +0100

    Create DiplomeDUT.class.php

---
+++ src/models/DiplomeDUT.class.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once(dirname(__FILE__)."/Diplome.class.php");
+
+class DiplomeDUT extends Diplome
+{
+    
+    private $departementIUT;
+    
+    public function __construct($id, $libelle, $departementIUT)
+    {
+        parent::__construct($id, $libelle);
+        $this->setDepartementIUT($departementIUT)
+    }
+    
+    //----------------------------------GETTERS------------------------------
+    public function getDepartementIUT()
+    {
+        return $this->departementIUT;
+    }
+    
+    //---------------------------------SETTERS------------------------------
+    public function setDepartementIUT($departementIUT)
+    {
+        if($departementIUT != null)
+        {
+            $this->departementDUT = $departementIUT;
+        }else{
+            throw new Exception("Departement IUT dans Diplome DUT est incorrect !");
+        }
+    }
+    
+    //---------------------------------toString---------------------------------
+    public function __toString()
+    {
+        return parent::__toString()." Departement IUT : ".$this->departementIUT;
+    }
+    
+}
+
+?>
