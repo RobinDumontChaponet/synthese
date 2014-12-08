4a00d4c5d714ff427eb6c7710b924e70c3baa92a
fadiliyoussef <fadiliYouss@gmail.com>
2014-12-08 20:59:37 +0100

    Create EstSpecialise.class.php

---
+++ src/models/EstSpecialise.class.php
@@ -0,0 +1,52 @@
+<?php
+
+class EstSpecialise
+{
+    private $ancien;
+    private $specialisation;
+    
+    public function __construct($ancien, $specialisation)
+    {
+        $this->setAncien($ancien);
+        $this->setSpecialisation($specialisation);
+    }
+    
+    //--------------------------------GETTERS--------------------
+    public function getAncien()
+    {
+        return $this->ancien;
+    }
+    
+    public function getSpecialisation()
+    {
+        return $this->specialisation;
+    }
+    
+    //------------------------------------SETTERS------------------
+    public function setAncien($ancien)
+    {
+        if($ancien != null)
+        {
+            $this->ancien = $ancien;
+        }else{
+            throw new Exception("Ancien dans EstSpecialise est incorrect !");
+        }
+    }
+    
+    public function setSpecialisation($specialisation)
+    {
+        if($specialisation != null)
+        {
+            $this->specialisation = $specialisation;
+        }else{
+            throw new Exception("Specialisation dans EstSpecialise est incorrecte !");
+        }
+    }
+    
+    //-----------------------------------toString---------------------------
+    public function __toString()
+    {
+        return "Ancien : ".$this->ancien->__toString()." Specialisation : ".$this->specialisation->__toString();
+    }
+    
+}
