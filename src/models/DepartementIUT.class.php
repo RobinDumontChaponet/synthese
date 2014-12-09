1756d4aeb8056e0ec943f54bddea90e3faf3751b
fadiliyoussef <fadiliYouss@gmail.com>
2014-12-09 21:14:37 +0100

    Create DepartementIUT.class.php

---
+++ src/models/DepartementIUT.class.php
@@ -0,0 +1,57 @@
+<?php
+
+class DepartementIUT
+{
+
+  private $id;
+  private $nom;
+
+  public function __construct($id, $nom)
+  {
+    $this->setId($id);
+    $this->setNom($nom);
+  }
+  
+  //--------------------------------GETTERS---------------------------------
+  public function getId()
+  {
+      return $this->id;
+  }
+  
+  public function getNom()
+  {
+    return $this->nom;
+  }
+  //--------------------------------SETTERS---------------------------------
+  public function setId($id)
+	{
+		if(($id != null) and ($id >= 0))
+		{
+			$this->id = $id;
+		}else
+		{
+			throw new Exception("Id departement IUT invalide");
+		}
+	}
+	
+	public function setNom($nom)
+	{
+		$nomTraite = trim($nom);
+		if(($nomTraite != null) and ($nomTraite != ""))
+		{
+			$this->nom = strtoupper($nomTraite);
+		}else
+		{
+			throw new Exception("Nom departement IUT incorrect");
+		}
+	}
+  //--------------------------------toString--------------------------------
+  
+  public function __toString()
+	{
+		return "Id : ".$this->id." Nom : ".$this->nom;
+	}
+
+}
+
+?>
