2afe5a1b94643315e9325baef4b8537cf159a759
RobinDumontChaponet <robin@dumontchapo.net>
2014-12-09 21:26:55 +0100

    indent

--- src/models/EntrepriseDAO.class.php
+++ src/models/EntrepriseDAO.class.php
@@ -1,54 +1,64 @@
 <?php
-    require_once("includes/dbConnection.inc.php");
-    require_once("models/EntrepriseAPE.class.php");
-    require_once("models/CodeAPE.class.php");
-    require_once("models/CodeAPEDAO.class.php");
+require_once("includes/dbConnection.inc.php");
+require_once("models/EntrepriseAPE.class.php");
+require_once("models/CodeAPE.class.php");
+require_once("models/CodeAPEDAO.class.php");
 
-    class EntrepriseDAO{
+class EntrepriseDAO
+{
+	public static function getAll()
+	{
+		$bdd=connect();
+		$req=$bdd->query("SELECT `idEntreprise`, `codeAPE`, `nom`, `adresse1`, `adresse2`, `codePostal`, `ville`, `cedex`, `pays`, `telephone` FROM `entreprise`");
+		$results=$req->fetchAll();
+		$lstEnt=array();
+		foreach ($results as $ent)
+		{
+			$lstEnt[]=new Entreprise($ent['idEntreprise'], $ent['nom'], $ent['adresse1'], $ent['adresse2'], $ent['codePostal'], $ent['ville'], $ent['cedex'], $ent['pays'], $ent['telephone'], CodeAPEDAO::getByID($ent['codeAPE']));
+		}
+		return $lstEnt;
+	}
 
-        public static function getAll(){
-            $bdd=connect();
-            $req=$bdd->query("SELECT `idEntreprise`, `codeAPE`, `nom`, `adresse1`, `adresse2`, `codePostal`, `ville`, `cedex`, `pays`, `telephone` FROM `entreprise`");
-            $results=$req->fetchAll();
-            $lstEnt=array();
-            foreach($results as $ent){
-                $lstEnt[]=new Entreprise($ent['idEntreprise'], $ent['nom'], $ent['adresse1'], $ent['adresse2'], $ent['codePostal'], $ent['ville'], $ent['cedex'], $ent['pays'], $ent['telephone'], CodeAPEDAO::getByID($ent['codeAPE']));
-            }
-            return $lstEnt;
-        }
+	public static function getById($id)
+	{
+		$bdd=connect();
+		$req=$bdd->prepare("SELECT `idEntreprise`, `codeAPE`, `nom`, `adresse1`, `adresse2`, `codePostal`, `ville`, `cedex`, `pays`, `telephone` FROM `entreprise` WHERE idEntreprise=?");
+		$ent=$req->fetch();
+		return new Entreprise($ent['idEntreprise'], $ent['nom'], $ent['adresse1'], $ent['adresse2'], $ent['codePostal'], $ent['ville'], $ent['cedex'], $ent['pays'], $ent['telephone'], CodeAPEDAO::getByID($ent['codeAPE']));
 
-        public static function getById($id){
-            $bdd=connect();
-            $req=$bdd->prepare("SELECT `idEntreprise`, `codeAPE`, `nom`, `adresse1`, `adresse2`, `codePostal`, `ville`, `cedex`, `pays`, `telephone` FROM `entreprise` WHERE idEntreprise=?");
-            $ent=$req->fetch();
-            return new Entreprise($ent['idEntreprise'], $ent['nom'], $ent['adresse1'], $ent['adresse2'], $ent['codePostal'], $ent['ville'], $ent['cedex'], $ent['pays'], $ent['telephone'], CodeAPEDAO::getByID($ent['codeAPE']));
+	}
 
-        }
+	public static function create($ent)
+	{
+		if
+		(gettype($ent)=="Entreprise")
+		{
+			$bdd->connect();
+			$req=$bdd->prepare("INSERT INTO `entreprise`(`codeAPE`, `nom`, `adresse1`, `adresse2`, `codePostal`, `ville`, `cedex`, `pays`, `telephone`) VALUES (?,?,?,?,?,?,?,?,?)");
+			$req->execute(array($ent->getCode(), $ent->getNom(), $ent->getAdresse1(), $ent->getAdresse2(), $ent->getCodePostal(), $ent->getVille(), $ent->getCedex(), $ent->getPays(), $ent->getTelephone()));
+			return $bdd->lastInsertId();
+		}
+	}
 
-        public static function create($ent){
-            if(gettype($ent)=="Entreprise"){
-                $bdd->connect();
-                $req=$bdd->prepare("INSERT INTO `entreprise`(`codeAPE`, `nom`, `adresse1`, `adresse2`, `codePostal`, `ville`, `cedex`, `pays`, `telephone`) VALUES (?,?,?,?,?,?,?,?,?)");
-                $req->execute(array($ent->getCode(), $ent->getNom(), $ent->getAdresse1(), $ent->getAdresse2(),$ent->getCodePostal(), $ent->getVille(), $ent->getCedex(), $ent->getPays(), $ent->getTelephone()));
-                return $bdd->lastInsertId();
-            }
-        }
+	public static function update($ent)
+	{
+		if (gettype($ent)=="Entreprise")
+		{
+			$bdd->connect();
+			$req=$bdd->prepare("UPDATE `entreprise` SET `codeAPE`=?,`nom`=?,`adresse1`=?,`adresse2`=?,`codePostal`=? ,`ville`=?,`cedex`=?,`pays`=?,`telephone`=? WHERE idEntreprise=?");
+			$req->execute(array($ent->getCode(), $ent->getNom(), $ent->getAdresse1(), $ent->getAdresse2(), $ent->getCodePostal(), $ent->getVille(), $ent->getCedex(), $ent->getPays(), $ent->getTelephone(), $ent->getId()));
+		}
+	}
 
-        public static function update($ent){
-            if(gettype($ent)=="Entreprise"){
-                $bdd->connect();
-                $req=$bdd->prepare("UPDATE `entreprise` SET `codeAPE`=?,`nom`=?,`adresse1`=?,`adresse2`=?,`codePostal`=? ,`ville`=?,`cedex`=?,`pays`=?,`telephone`=? WHERE idEntreprise=?");
-                $req->execute(array($ent->getCode(), $ent->getNom(), $ent->getAdresse1(), $ent->getAdresse2(),$ent->getCodePostal(), $ent->getVille(), $ent->getCedex(), $ent->getPays(), $ent->getTelephone(), $ent->getId()));
-            }
-        }
+	public static function delete($ent)
+	{
+		if (gettype($ent)=="Entreprise")
+		{
+			$bdd->connect();
+			$req=$bdd->prepare("DELETE FROM `entreprise` WHERE `idEntreprise`=?");
+			$req->execute(array($ent->getId()));
+		}
+	}
 
-        public static function delete($ent){
-             if(gettype($ent)=="Entreprise"){
-                $bdd->connect();
-                $req=$bdd->prepare("DELETE FROM `entreprise` WHERE `idEntreprise`=?");
-                 $req->execute(array($ent->getId()));
-             }
-        }
-
-    }
+}
 ?>
--- src/models/EstSpecialise.class.php
+++ src/models/EstSpecialise.class.php
@@ -4,49 +4,49 @@ class EstSpecialise
 {
     private $ancien;
     private $specialisation;
-    
+
     public function __construct($ancien, $specialisation)
     {
         $this->setAncien($ancien);
         $this->setSpecialisation($specialisation);
     }
-    
+
     //--------------------------------GETTERS--------------------
     public function getAncien()
     {
         return $this->ancien;
     }
-    
+
     public function getSpecialisation()
     {
         return $this->specialisation;
     }
-    
+
     //------------------------------------SETTERS------------------
     public function setAncien($ancien)
     {
         if($ancien != null)
         {
             $this->ancien = $ancien;
-        }else{
+        } else {
             throw new Exception("Ancien dans EstSpecialise est incorrect !");
         }
     }
-    
+
     public function setSpecialisation($specialisation)
     {
         if($specialisation != null)
         {
             $this->specialisation = $specialisation;
-        }else{
+        } else {
             throw new Exception("Specialisation dans EstSpecialise est incorrecte !");
         }
     }
-    
+
     //-----------------------------------toString---------------------------
     public function __toString()
     {
-        return "Ancien : ".$this->ancien->__toString()." Specialisation : ".$this->specialisation->__toString();
+        return "Ancien : ".$this->ancien." Specialisation : ".$this->specialisation;
     }
-    
+
 }
