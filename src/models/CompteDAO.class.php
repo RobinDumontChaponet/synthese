793127c46bbdf0f0fab5fe0aa697e6d39850e666
lmaginot <mathieu@lalignemaginot.fr>
2014-12-10 15:57:48 +0100

    compteDAO, tkt robin je ne fait pas un commit par fichier à partir de mnt, je fait tout et je commit le tout ;)

--- src/models/CompteDAO.class.php
+++ src/models/CompteDAO.class.php
@@ -1,7 +1,7 @@
 <?php
 require_once("includes/dbConnection.inc.php");
-require_once("includes/Compte.class.php");
-require_once("includes/PersonneDAO.class.php");
+require_once("models/Compte.class.php");
+require_once("models/PersonneDAO.class.php");
 
     class CompteDAO{
 
@@ -25,19 +25,55 @@ require_once("includes/PersonneDAO.class.php");
         }
 
         public static function getAll(){
-
+            $lstCompte=array();
+            try{
+                $bdd=connect();
+                $req=$bdd->query("SELECT * FROM compte ORDER BY ndc");
+                while($res=$req->fetch(PDO::FETCH_OBJ)){
+                    $Personne=PersonneDAO::getById($res->idPersonne());
+                    $lstCompte[]=new Compte($res->idCompte, $res->idProfil, $Personne, $res->ndc, $res->mdp);
+                }
+            }catch (PDOException $e) {
+                die("Error getAll() !: " . $e->getMessage() . "<br/>");
+            }
+            return $lstCompte;
         }
 
-        public static function getById(){
+        public static function getById($id){
+            $compte = NULL;
+            try {
+                $connect = connect();
+                $statement = $connect->prepare("SELECT * FROM compte WHERE idCompte=?");
+                $statement->execute(array($id));
+                    if ($res = $statement->fetch(PDO::FETCH_OBJ)) {
+                        $Personne=PersonneDAO::getById($res->idPersonne());
+                        $compte=new Compte($res->idCompte, $res->idProfil, $Personne, $res->ndc, $res->mdp);
+                    }
 
+            } catch (PDOException $e) {
+                die("Error getCompteById() !: " . $e->getMessage() . "<br/>");
+            }
+            return $compte;
         }
 
-        public static function update(){
-
+        public static function update($compte){
+            try{
+                $bdd=connect();
+                $req=$bdd->prepare("UPDATE `compte` SET `idProfil`=?,`idPersonne`=?,`ndc`=?,`mdp`=? WHERE `idCompte`=?");
+                $req->execute(array($compte->getTypeProfil(), $compte->getPersonne()->getId(),$compte->getMdp(),$compte->getId()));
+            }catch (PDOException $e) {
+                die("Error update() !: " . $e->getMessage() . "<br/>");
+            }
         }
 
-        public static function delete(){
-
+        public static function delete($compte){
+            try{
+                $bdd=$connect();
+                $req=$bdd->prepare("DELETE FROM `compte` WHERE `idCompte?");
+                $req->execute(array($compte->getId()));
+            }catch (PDOException $e) {
+                die("Error delete() !: " . $e->getMessage() . "<br/>");
+            }
         }
 
     }
