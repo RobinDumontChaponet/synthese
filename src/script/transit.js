77dd7e983019f8e715567b4c1b0a1595737306d6
RobinDumontChaponet <robin@dumontchapo.net>
2014-12-11 20:19:50 +0100

    natural sort

--- src/script/transit.js
+++ src/script/transit.js
@@ -25,16 +25,34 @@ function arrayToTable(tableData, headers) {
 	return table;
 }
 
-var csvColName = { 'nomUsage':'Nom d\'usage', 'nomPat':'Nom patronymique', 'prenom':'Prénom', 'dateNais':'Date de naissance', 'adresse':'Adresse postale', 'codePost':'Code postal', 'ville':'Ville', 'pays':'Pays', 'mail':'Adresse e-mail', 'telMob':'Téléphone mobile', 'telFix':'Téléphone fixe' };
+var csvColName = [
+	{key:'nomUsage', value:'Nom d\'usage'},
+	{key:'nomPat', value:'Nom patronymique'},
+	{key:'prenom', value:'Prénom'},
+	{key:'dateNais', value:'Date de naissance'},
+	{key:'adresse', value:'Adresse postale'},
+	{key:'codePost', value:'Code postal'},
+	{key:'ville', value:'Ville'},
+	{key:'pays', value:'Pays'},
+	{key:'mail', value:'Adresse e-mail'},
+	{key:'telMob', value:'Téléphone mobile'},
+	{key:'telFix', value:'Téléphone fixe'}
+];
+
+csvColName.sort(function(a, b) {
+	return a.value.localeCompare(b.value);
+});
+
+//var csvColName = { 'nomUsage':'Nom d\'usage', 'nomPat':'Nom patronymique', 'prenom':'Prénom', 'dateNais':'Date de naissance', 'adresse':'Adresse postale', 'codePost':'Code postal', 'ville':'Ville', 'pays':'Pays', 'mail':'Adresse e-mail', 'telMob':'Téléphone mobile', 'telFix':'Téléphone fixe' };
+
 
 function csvArrayToTable(array) {
 	var headers=Array();
 	for(var i=0, l=array[0].length; i<l; i++) {
-
 		var select = '<select name="col'+i+'"><option value="" disabled selected style="display:none;">Type</option><option value="unused" class="unusedOption"> (Inutilisé)</option>';
-		for(var key in csvColName)
-			select += '<option value="'+key+'">'+csvColName[key]+'</option>';
-			headers.push(select);
+		for(var k=0, ll=csvColName.length; k<ll; k++)
+			select += '<option value="'+csvColName[k].key+'">'+csvColName[k].value+'</option>';
+		headers.push(select);
 	}
 	return arrayToTable(array, headers);
 }
