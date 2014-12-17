b718eb6c2d90cbfd158a31011489405031c005b6
RobinDumontChaponet <robin@dumontchapo.net>
2014-12-17 15:23:41 +0100

    correction .= pour concat (+indent auto)

--- src/models/AncienDAO.class.php
+++ src/models/AncienDAO.class.php
@@ -17,22 +17,22 @@ class AncienDAO
 	 */
 	public static function getAll()
 	{
-        try{
-            // appelle de la connexion
-            $bdd=connect();
-            $req=$bdd->query("SELECT `idAncien`, A.idPersonne, `adresse1`, `adresse2`, `codePostal`, `ville`, `pays`, `mobile`, `telephone`, `imageProfil`, `imageTrombi`,`idCompte`,`nomUsage`,`nomPatronymique`,`prenom`, `mail` FROM `ancien` A, `personne` P WHERE P.idPersonne=A.idPersonne ORDER BY nomUsage");
-            $lst=$req->fetchAll();
-            $lstAncien=array();
-            foreach ($lst as $ancien)
-            {
-                $ancienObj=new Ancien($ancien['idPersonne'], $ancien['nomUsage'], $ancien['nomPatronymique'], $ancien['prenom'], $ancien['adresse1'], $ancien['adresse2'], $ancien['codePostal'], $ancien['ville'], $ancien['pays'], $ancien['mobile'], $ancien['telephone'], $ancien['imageProfil'], $ancien['imageTrombi']);
-                $lstAncien[]=$ancienObj;
-            }
+		try{
+			// appelle de la connexion
+			$bdd=connect();
+			$req=$bdd->query("SELECT `idAncien`, A.idPersonne, `adresse1`, `adresse2`, `codePostal`, `ville`, `pays`, `mobile`, `telephone`, `imageProfil`, `imageTrombi`,`idCompte`,`nomUsage`,`nomPatronymique`,`prenom`, `mail` FROM `ancien` A, `personne` P WHERE P.idPersonne=A.idPersonne ORDER BY nomUsage");
+			$lst=$req->fetchAll();
+			$lstAncien=array();
+			foreach ($lst as $ancien)
+			{
+				$ancienObj=new Ancien($ancien['idPersonne'], $ancien['nomUsage'], $ancien['nomPatronymique'], $ancien['prenom'], $ancien['adresse1'], $ancien['adresse2'], $ancien['codePostal'], $ancien['ville'], $ancien['pays'], $ancien['mobile'], $ancien['telephone'], $ancien['imageProfil'], $ancien['imageTrombi']);
+				$lstAncien[]=$ancienObj;
+			}
 
-            return $lstAncien;
-        }catch(PDOException $e){
-            die('error get all ancien '.$e->getMessage().'<br>');
-        }
+			return $lstAncien;
+		}catch(PDOException $e){
+			die('error get all ancien '.$e->getMessage().'<br>');
+		}
 
 	}
 
@@ -45,16 +45,16 @@ class AncienDAO
 	{
 		if (is_numeric($id))
 		{
-            try{
-                $bdd=connect();
-                $req=$bdd->prepare("SELECT `idAncien`, A.idPersonne, `adresse1`, `adresse2`, `codePostal`, `ville`, `pays`, `mobile`, `telephone`, `imageProfil`, `imageTrombi`,`idCompte`,`nomUsage`,`nomPatronymique`,`prenom`, `mail` FROM `ancien` A, `personne` P WHERE P.idPersonne=A.idPersonne AND A.idPersonne=? ORDER BY nomUsage");
-                $req->execute(array($id));
-                $ancien=$req->fetch();
-                $ancienObj=new Ancien($ancien['idPersonne'], $ancien['nomUsage'], $ancien['nomPatronymique'], $ancien['prenom'], $ancien['adresse1'], $ancien['adresse2'], $ancien['codePostal'], $ancien['ville'], $ancien['pays'], $ancien['mobile'], $ancien['telephone'], $ancien['imageProfil'], $ancien['imageTrombi']);
-                return $ancienObj;
-            }catch(PDOException $e){
-                die('error get id ancien '.$e->getMessage().'<br>');
-            }
+			try{
+				$bdd=connect();
+				$req=$bdd->prepare("SELECT `idAncien`, A.idPersonne, `adresse1`, `adresse2`, `codePostal`, `ville`, `pays`, `mobile`, `telephone`, `imageProfil`, `imageTrombi`,`idCompte`,`nomUsage`,`nomPatronymique`,`prenom`, `mail` FROM `ancien` A, `personne` P WHERE P.idPersonne=A.idPersonne AND A.idPersonne=? ORDER BY nomUsage");
+				$req->execute(array($id));
+				$ancien=$req->fetch();
+				$ancienObj=new Ancien($ancien['idPersonne'], $ancien['nomUsage'], $ancien['nomPatronymique'], $ancien['prenom'], $ancien['adresse1'], $ancien['adresse2'], $ancien['codePostal'], $ancien['ville'], $ancien['pays'], $ancien['mobile'], $ancien['telephone'], $ancien['imageProfil'], $ancien['imageTrombi']);
+				return $ancienObj;
+			}catch(PDOException $e){
+				die('error get id ancien '.$e->getMessage().'<br>');
+			}
 		}
 	}
 
@@ -62,33 +62,33 @@ class AncienDAO
 	{
 		if (gettype($ancien)=="Ancien")
 		{
-            try{
-                $bdd=connect();
-                $idPers=PersonneDAO::create(new Personne(0, $ancien->getNom(), $ancien->getNomPatronymique(), $ancien->getPrenom(), $ancien->getMail()));
-                $req=$bdd->prepare("INSERT INTO `ancien`(`idPersonne`, `adresse1`, `adresse2`, `codePostal`, `ville`, `pays`, `mobile`, `telephone`, `imageProfil`, `imageTrombi`) VALUES (?,?,?,?,?,?,?,?,?,?)");
-                $req->execute(array($idPers, $ancien->getAdresse1(), $ancien->getAdresse2(), $ancien->getCodePostal, $ancien->getVille(), $ancien->getPays(), $ancien->getMobile(), $ancien->getTelephone(), $ancien->getImageProfil(), $ancien->getImageTrombi()));
-            }catch(PDOException $e){
-                die('error create ancien '.$e->getMessage().'<br>');
-            }
+			try{
+				$bdd=connect();
+				$idPers=PersonneDAO::create(new Personne(0, $ancien->getNom(), $ancien->getNomPatronymique(), $ancien->getPrenom(), $ancien->getMail()));
+				$req=$bdd->prepare("INSERT INTO `ancien`(`idPersonne`, `adresse1`, `adresse2`, `codePostal`, `ville`, `pays`, `mobile`, `telephone`, `imageProfil`, `imageTrombi`) VALUES (?,?,?,?,?,?,?,?,?,?)");
+				$req->execute(array($idPers, $ancien->getAdresse1(), $ancien->getAdresse2(), $ancien->getCodePostal, $ancien->getVille(), $ancien->getPays(), $ancien->getMobile(), $ancien->getTelephone(), $ancien->getImageProfil(), $ancien->getImageTrombi()));
+			}catch(PDOException $e){
+				die('error create ancien '.$e->getMessage().'<br>');
+			}
 		}else{
-            die('paramètre de type ancien requis');
-        }
+			die('paramètre de type ancien requis');
+		}
 	}
 
 	public static function update($ancien)
 	{
 		if (gettype($ancien)=="Ancien")
 		{
-            try{
-                $bdd=connect();
-                PersonneDAO::update(new Personne($ancien->getId(), $ancien->getNom(), $ancien->getNomPatronymique(), $ancien->getPrenom(), $ancien->getMail()));
-                $req=$bdd->prepare("UPDATE `ancien` SET `adresse1`=?,`adresse2`=?,`codePostal`=?,`ville`=? ,`pays`=?,`mobile`=?,`telephone`=?,`imageProfil`=?,`imageTrombi`=? WHERE `idPersonne`=?");
-                $req->execute(array($ancien->getAdresse1(), $ancien->getAdresse2(), $ancien->getCodePostal, $ancien->getVille(), $ancien->getPays(), $ancien->getMobile(), $ancien->getTelephone(), $ancien->getImageProfil(), $ancien->getImageTrombi(), $ancien->getId()));
-            }catch(PDOException $e){
-                die('error update ancien '.$e->getMessage().'<br>');
-            }
+			try{
+				$bdd=connect();
+				PersonneDAO::update(new Personne($ancien->getId(), $ancien->getNom(), $ancien->getNomPatronymique(), $ancien->getPrenom(), $ancien->getMail()));
+				$req=$bdd->prepare("UPDATE `ancien` SET `adresse1`=?,`adresse2`=?,`codePostal`=?,`ville`=? ,`pays`=?,`mobile`=?,`telephone`=?,`imageProfil`=?,`imageTrombi`=? WHERE `idPersonne`=?");
+				$req->execute(array($ancien->getAdresse1(), $ancien->getAdresse2(), $ancien->getCodePostal, $ancien->getVille(), $ancien->getPays(), $ancien->getMobile(), $ancien->getTelephone(), $ancien->getImageProfil(), $ancien->getImageTrombi(), $ancien->getId()));
+			}catch(PDOException $e){
+				die('error update ancien '.$e->getMessage().'<br>');
+			}
 		}else{
-            die('paramètre de type ancien requis');
+			die('paramètre de type ancien requis');
 		}
 	}
 
@@ -96,80 +96,89 @@ class AncienDAO
 	{
 		if (gettype($ancien)=="Ancien")
 		{
-            try{
-                $bdd=connect();
-                PersonneDAO::delete(new Personne($ancien->getId(), $ancien->getNom(), $ancien->getNomPatronymique(), $ancien->getPrenom(), $ancien->getMail()));
-                $req=$bdd->prepare("DELETE FROM `ancien` WHERE `idPersonne`=?");
-                $req->execute(array($ancien->getId()));
-            }catch(PDOException $e){
-                die('error delete ancien '.$e->getMessage().'<br>');
-            }
+			try{
+				$bdd=connect();
+				PersonneDAO::delete(new Personne($ancien->getId(), $ancien->getNom(), $ancien->getNomPatronymique(), $ancien->getPrenom(), $ancien->getMail()));
+				$req=$bdd->prepare("DELETE FROM `ancien` WHERE `idPersonne`=?");
+				$req->execute(array($ancien->getId()));
+			}catch(PDOException $e){
+				die('error delete ancien '.$e->getMessage().'<br>');
+			}
 		}else{
-            die('paramètre de type ancien requis');
+			die('paramètre de type ancien requis');
 		}
 	}
 
-      /**
-     * Recherche une liste d'ancien dans la base de donnée
-     * @param   String   $nom         [[Description]]
-     * @param   String   $prn         [[Description]]
-     * @param   Array    $promo       [[Description]]
-     * @param   Object   $diplome     [[Description]]
-     * @param   Object   $spe         [[Description]]
-     * @param   Object   $typeSpe     [[Description]]
-     * @param   Object   $PostDut     [[Description]]
-     * @param   Object   $etabPostDut [[Description]]
-     * @param   Boolean $trav        [[Description]]
-     * @returns Object   [[Description]]
-     */
-    public static function search($nom,$prn,$promo,$diplome,$spe,$typeSpe,$PostDut,$etabPostDut,$trav){
-        $lst=array();
-        $args=array();
-        $req="SELECT `idAncien`, A.idPersonne, `adresse1`, `adresse2`, `codePostal`, `ville`, `pays`, `mobile`, `telephone`, `imageProfil`, `imageTrombi`,`idCompte`,`nomUsage`,`nomPatronymique`,`prenom`, `mail` FROM `ancien` A, `personne` P,`aEtudie` Etud, `estSpecialise` Spe, `Specialisation` Special,`Possede` Poss WHERE P.idPersonne=A.idPersonne ";
-        if($nom != null){
-            $req.=" AND P.nomUsage LIKE %?% ";
-            $args[]=$nom;
-        }
-        if($prn!=null){
-            $req.=" AND P.prenom LIKE %?% ";
-            $args[]=$nom;
-        }
-        if($promo!=null){
-            $req.=" AND P.idPersonne=Etud.idPersonne AND Etud.idPromo=? ";
-            $args[]=$promo->getId();
-        }
-        if($diplome!=null){
-            $req.=" AND P.idPersonne=Etud.idPersonne AND idDiplomeDUT=? ";
-            $args[]=$diplome->getId();
-        }
-        if($spe!=null){
-            $req.=" AND P.idPersonne=Spe.idPersonne AND Spe.idSpe=? ";
-            $args[]=$spe->getId();
-        }
-        if($typeSpe!=null){
-            $req.=" AND P.idPersonne=Spe.idPersonne AND Spe.idSpe=Special.idSpe AND Special.idTypeSpe=? ";
-            $args[]=$typeSpe->getId();
-        }
-        if($PostDut!=null){
-            $req=" AND P.idPersonne=Poss.idPersonne AND Poss.idDiplomePost=? ";
-            $args[]=$PostDut->getId();
-        }
-        if($etabPostDut!=null){
-            $req=" AND P.idPersonne=Poss.idPersonne AND Poss.idEtablissement=? ";
-            $args[]=$etabPostDut->getId();
-        }
-        try{
-            $bdd=connect();
-            $state=$bdd->prepare($req);
-            $state->execute($args);
-            while($ancien=$state->fetch()){
-                $lst[]=new Ancien($ancien['idPersonne'], $ancien['nomUsage'], $ancien['nomPatronymique'], $ancien['prenom'], $ancien['adresse1'], $ancien['adresse2'], $ancien['codePostal'], $ancien['ville'], $ancien['pays'], $ancien['mobile'], $ancien['telephone'], $ancien['imageProfil'], $ancien['imageTrombi']);
-            }
-        }catch(PDOException $e){
-            die('error search ancien '.$e->getMessage().'<br>');
-        }
-        return $lst;
-    }
-
+	/**
+	 * Recherche une liste d'ancien dans la base de donnée
+	 * @param   String   $nom         [[Description]]
+	 * @param   String   $prn         [[Description]]
+	 * @param   Array    $promo       [[Description]]
+	 * @param   Object   $diplome     [[Description]]
+	 * @param   Object   $spe         [[Description]]
+	 * @param   Object   $typeSpe     [[Description]]
+	 * @param   Object   $PostDut     [[Description]]
+	 * @param   Object   $etabPostDut [[Description]]
+	 * @param   Boolean $trav        [[Description]]
+	 * @returns Object   [[Description]]
+	 */
+	public static function search($nom, $prn, $promo, $diplome, $spe, $typeSpe, $PostDut, $etabPostDut, $trav)
+	{
+		$lst=array();
+		$args=array();
+		$req="SELECT `idAncien`, A.idPersonne, `adresse1`, `adresse2`, `codePostal`, `ville`, `pays`, `mobile`, `telephone`, `imageProfil`, `imageTrombi`,`idCompte`,`nomUsage`,`nomPatronymique`,`prenom`, `mail` FROM `ancien` A, `personne` P,`aEtudie` Etud, `estSpecialise` Spe, `Specialisation` Special,`Possede` Poss WHERE P.idPersonne=A.idPersonne ";
+		if($nom != null)
+		{
+			$req.=" AND P.nomUsage LIKE %?% ";
+			$args[]=$nom;
+		}
+		if($prn!=null)
+		{
+			$req.=" AND P.prenom LIKE %?% ";
+			$args[]=$nom;
+		}
+		if($promo!=null)
+		{
+			$req.=" AND P.idPersonne=Etud.idPersonne AND Etud.idPromo=? ";
+			$args[]=$promo->getId();
+		}
+		if($diplome!=null)
+		{
+			$req.=" AND P.idPersonne=Etud.idPersonne AND idDiplomeDUT=? ";
+			$args[]=$diplome->getId();
+		}
+		if($spe!=null)
+		{
+			$req.=" AND P.idPersonne=Spe.idPersonne AND Spe.idSpe=? ";
+			$args[]=$spe->getId();
+		}
+		if($typeSpe!=null)
+		{
+			$req.=" AND P.idPersonne=Spe.idPersonne AND Spe.idSpe=Special.idSpe AND Special.idTypeSpe=? ";
+			$args[]=$typeSpe->getId();
+		}
+		if($PostDut!=null)
+		{
+			$req.=" AND P.idPersonne=Poss.idPersonne AND Poss.idDiplomePost=? ";
+			$args[]=$PostDut->getId();
+		}
+		if($etabPostDut!=null)
+		{
+			$req.=" AND P.idPersonne=Poss.idPersonne AND Poss.idEtablissement=? ";
+			$args[]=$etabPostDut->getId();
+		}
+		try{
+			$bdd=connect();
+			$state=$bdd->prepare($req);
+			$state->execute($args);
+			while($ancien=$state->fetch())
+			{
+				$lst[]=new Ancien($ancien['idPersonne'], $ancien['nomUsage'], $ancien['nomPatronymique'], $ancien['prenom'], $ancien['adresse1'], $ancien['adresse2'], $ancien['codePostal'], $ancien['ville'], $ancien['pays'], $ancien['mobile'], $ancien['telephone'], $ancien['imageProfil'], $ancien['imageTrombi']);
+			}
+		}catch(PDOException $e){
+			die('error search ancien '.$e->getMessage().'<br>');
+		}
+		return $lst;
+	}
 }
 ?>
