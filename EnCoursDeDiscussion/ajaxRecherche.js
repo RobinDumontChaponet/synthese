0755a31afeccb77f60429099d719ebf236bf9c77
fadiliyoussef <fadiliYouss@gmail.com>
2014-12-18 19:59:51 +0100

    Update ajaxRecherche.js

--- EnCoursDeDiscussion/ajaxRecherche.js
+++ EnCoursDeDiscussion/ajaxRecherche.js
@@ -35,7 +35,7 @@ function link_ajax()
             var etablissementpostdut = document.getElementById("etabPostDUT").value;
             var travailactuel = document.getElementById("travailActuel").value;
             
-            var chemin = $_SERVER['SERVER_NAME']."/~dumont28u/synthese/src/helpers/search.php";
+            var chemin = $_SERVER['SERVER_NAME']+"/~dumont28u/synthese/src/helpers/search.php";
             xhr.open("GET", chemin+"?nom="+nom+"&prenom="+prenom+"&promotion="+promotion+"&diplomedut="+diplomedut+"&typesspecialisations="+typespecialisations+"&specialisation="+specialisation+"&diplomepostdut="+diplomepostdut+"&etablissementpostdut="+etablissementpostdut+"&travailactuel="+travailactuel, true);
             
             xhr.onreadystatechange = affichageResultat;
