0dba98a481322bd4b1766c3a5e1266795dd4da73
JozwickiVictor <tigre.demon@hotmail.fr>
2014-12-18 23:34:02 +0100

    Commit temporaire

--- src/views/profil.php
+++ src/views/profil.php
@@ -10,7 +10,7 @@
 			<form action="index.php?modifier" method="post">
 				<?php //if nom d'usage existe se démerder mais comme j'ai rien qui marche ...?>
 				<li><label form="lastNamePatro">Nom patronymique:</label><input type="text" value="getNom()"><label form="lastName">Nom d'usage:</label><input type="text" value="getNom()"><label form="firstName">Prénom :</label><input type="text" value="getPrenom()"></li>
-				<li><label form="birthday">Date de naissance : </label><input type="text" value ="get</li>
+				<li><label form="birthday">Date de naissance : </label><input type="text" value ="getNaissance()"><label form="sex">Sexe : </label><input type="text" value ="getSexe()"></li>
 				<li><label form="adress1">Adresse :</label><input type="text" value="getAdresse1()"> <label form="postalCode">Code postal :</label><input type="text" value="getCodePostal()"></li>
 				<?php // if adress2 exist then show it, else don't ...?>
 				<li><label form="city">Ville :</label><input type="text" value="getVille()"><label form="country">Pays :</label><input type="text" value="getPays()"></li>
