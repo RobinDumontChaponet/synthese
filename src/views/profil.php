095252610616e7cbd699f5cb71f58ce90137fea4
JozwickiVictor <tigre.demon@hotmail.fr>
2014-12-19 01:10:10 +0100

    changes

--- src/views/profil.php
+++ src/views/profil.php
@@ -12,7 +12,7 @@
 				<li><label for="lastNamePatro">Nom patronymique:</label><input type="text" value="$ancien->getNom()"/><label for="lastName">Nom d'usage:</label><input type="text" value="$ancien->getNom()"/><label for="firstName">Prénom :</label><input type="text" value="$ancien->getPrenom()"/></li>
 				<li><label for="birthday">Date de naissance : </label><input type="text" value ="$ancien->getNaissance()"/><label for="sex">Sexe : </label><input type="text" value ="$ancien->getSexe()"/></li>
 				<li><label for="adress1">Adresse :</label><input type="text" value="$ancien->getAdresse1()"/> <label for="postalCode">Code postal :</label><input type="text" value="$ancien->getCodePostal()"/></li>
-				<!--<?php //if ($ancien->getAdresse2() != NULL)?>
+				<!--<?php //if ($adresse2)?>
 					<li><label for="">Adresse 2 : </label><input type="text" value="$ancien->getAdresse2()"/></li>-->
 				<li><label for="city">Ville :</label><input type="text" value="$ancien->getVille()"/><label for="country">Pays :</label><input type="text" value="$ancien->getPays()"/></li>
 				<li><label for="phoneNumber">Telephone :</label><input type="text" value="$ancien->getTelephone()"/><label for="mobileNumber">Mobile :</label><input type="text" value="$ancien->getMobile()"/></li>
