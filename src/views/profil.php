c45c2cd7097ef578f083e7c210d34c5e4144fc7e
JozwickiVictor <tigre.demon@hotmail.fr>
2014-12-20 23:18:35 +0100

    profil image

--- src/views/profil.php
+++ src/views/profil.php
@@ -13,7 +13,7 @@
 				echo '<img height="230px" width="200px" src="data:image/jpg;base64,'.base64_encode($imageProfil).'" alt="Image de trombinoscope"/>'; 
 			else
 				echo '<img src="style/images/nobody.png" alt="Pas d\'image de trombi"/>';*/?>
-			<img src="index.php?requ=viewImage" alt="test"/>
+			<img src="helpers/viewImage.php?id=1" alt="test"/>
 			<!--<input type="file" name="imageProfil"/><input type="submit" name="submit" value="Upload"/>-->
 		</fieldset>
 		<fieldset>
--- src/views/viewImage.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-$personne = PersonneDAO::getById($_SESSION[syntheseUser]->getId());
-$ancien = AncienDAO::getById($personne->getId());
-$imageProfil = $ancien->getImageProfil();
-$imageTrombi = $ancien->getImageTrombi();
-
-("Content-type: image/jpg");
-echo $imageTrombi;
-?>
