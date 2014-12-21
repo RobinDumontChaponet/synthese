515b92eddf1d2c1d87667dcad666cdc234cd9e1b
JozwickiVictor <tigre.demon@hotmail.fr>
2014-12-21 18:04:12 +0100

    test2

---
+++ src/helpers/imageProfilTest2.php
@@ -0,0 +1,19 @@
+﻿<?php
+include('conf.inc.php');
+include_once(MODELS_INC.'AncienDAO.class.php');
+include_once(MODELS_INC.'PersonneDAO.class.php');
+
+if (isset($_GET['id']) && $_GET['id'] != NULL) {
+	$personne = PersonneDAO::getById($_GET['id']);
+	$ancien = AncienDAO::getById($personne->getId());
+	if ($ancien != NULL)
+		$imageProfil = $ancien->getImageProfil();
+	else
+		echo 'Aucune image de profil existante pour cet id';
+	
+header('Content-Type: image/jpeg');
+echo $imageProfil;
+} else {
+	echo 'Aucun id n\'a été mit';
+}
+?>
