b5650ae7ccb7686655161bc10da525ab7bb7cb09
JozwickiVictor <tigre.demon@hotmail.fr>
2014-12-22 14:06:55 +0100

    Yeay on peut voir la liste des gens de la promo

--- src/controllers/promotion.php
+++ src/controllers/promotion.php
@@ -1,4 +1,8 @@
 <?php
-
+if (isset($_GET['id'])) {
+	$promo = PromotionDAO::getById($_GET['id']);
+	$anciens = AncienDAO::getAncienByIdPromo($_GET['id']);
+} else
+	$anciens = NULL;
 include(VIEWS_INC.'promotion.php');
 ?>
--- src/views/promotion.php
+++ src/views/promotion.php
@@ -1,6 +1,22 @@
 ﻿<!--meta title="IUTbook | Promotions" css="style/animations.css"-->
 
 <section id="content">
-	<p>C'est la promo avec l'id <?php echo $_GET['id'];?></p>
+	<h1 style="color:lightBlue">Page de la promotion année <?php echo $promo->getAnnee();?></h1>
+	<div>
+		<p>ici on des news de la promo ici on des news de la promo ici on des news de la promo ici on des news de la promo ici on des news de la promo ici on des news de la promo ici on des news de la promo</p>
+	</div>
+	<fieldset>
+		<legend style="color:lightBlue">Liste des étudiants de la promotion</legend>
+		<ol>
+			<?php 
+			if($anciens != NULL) {
+				foreach($anciens as $ancien) {
+					echo '<li><a href="?requ=profil&id='.$ancien->getId().'">'.$ancien->getNomPatronymique().' '.$ancien->getPrenom().'</a></li>';
+				}
+			} else {
+				echo '<p>Il n\'y a pas d\'étudiants dans cette promotion</p>';
+			}?>
+		</ol>
+	</fieldset>
 	
 </section>
