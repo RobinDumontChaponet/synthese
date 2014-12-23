59d630c1ca76f217a2abaa52db640630ba820bb8
RobinDumontChaponet <robin@dumontchapo.net>
2014-12-23 17:09:11 +0100

    permissions (j'en avais oublié un ^^)

--- src/helpers/upload-csv.php
+++ src/helpers/upload-csv.php
@@ -1,41 +1,49 @@
 <?php
-/*include_once(dirname(__FILE__).'/models/User.class.php');
-session_start();
-if (!isset($_SESSION['trombiUser']) || $_SESSION['trombiUser']=='' || $_SESSION['trombiUser']->getAuth()->getId()!=0) {
-	exit();
-}*/
-
 include('conf.inc.php');
-include('urls.transit.inc.php');
 
-$possibleDestinations = array('data/csv');
+session_start();
 
-$possibleExtensions = array('csv');
+if($_SESSION["syntheseUser"]) {
 
-$info=pathinfo($_FILES['upload']['name']);
-if($_REQUEST['basename']!='')
-	$info['basename']=post_slug($_REQUEST['basename']);
-else
-	$info['basename']=post_slug($info['basename']);
+	include('urls.transit.inc.php');
 
-$fileType = $_FILES['upload']['type'];
-$destination = $_REQUEST['destination'];
+	// Définitions des droits
+	include_once(MODELS_INC."DisposeDeDAO.class.php");
+	$user_auth = DisposeDeDAO::getByTypeProfilAndPage($_SESSION["syntheseUser"]->getTypeProfil(), PageDAO::getByLibelle('csv-import'))->getDroit();
 
-if( !in_array($destination, $possibleDestinations))
-	die('bad path ! ;-)');
+	if($user_auth['write']) {
 
-if ( in_array(strtolower($info['extension']), $possibleExtensions) ) {
+		$possibleDestinations = array('data/csv');
 
-	$fileContent = file_get_contents($_FILES['upload']['tmp_name']);
-	if(file_put_contents('../'.$destination.$sub.'/'.$info['basename'], $fileContent)!==false) {
-		echo json_encode(array(
-		  'name' => $info['basename'],
-		  'type' => $fileType,
-		  'path' => $destination.$sub,
-		  'ext' => $info['extension']
-		));
+		$possibleExtensions = array('csv');
+
+		$info=pathinfo($_FILES['upload']['name']);
+		if($_REQUEST['basename']!='')
+			$info['basename']=post_slug($_REQUEST['basename']);
+		else
+			$info['basename']=post_slug($info['basename']);
+
+		$fileType = $_FILES['upload']['type'];
+		$destination = $_REQUEST['destination'];
+
+		if( !in_array($destination, $possibleDestinations))
+			die('bad path ! ;-)');
+
+		if ( in_array(strtolower($info['extension']), $possibleExtensions) ) {
+
+			$fileContent = file_get_contents($_FILES['upload']['tmp_name']);
+			if(file_put_contents('../'.$destination.$sub.'/'.$info['basename'], $fileContent)!==false) {
+				echo json_encode(array(
+					'name' => $info['basename'],
+					'type' => $fileType,
+					'path' => $destination.$sub,
+					'ext' => $info['extension']
+				));
+			}
+
+		} else
+			die('unnacepted extension');
+
 	}
-
-} else
-	die('unnacepted extension');
+}
 ?>
