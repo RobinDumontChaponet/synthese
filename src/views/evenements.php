296c7e1eec5e08775c796d1c19c66d89e649e822
RobinDumontChaponet <robin@dumontchapo.net>
2014-12-28 07:38:29 +0100

    style

--- src/views/evenements.php
+++ src/views/evenements.php
@@ -1,80 +1,87 @@
-﻿<!--meta title="Événements" css="style/animations.css"-->
-
+﻿<!--meta title="Événements" css="style/animations.css" css="style/evenements.css"-->
 <div id="content">
 	<?php if ($user == "Ancien" || $user == "Professeur") {?>
 		<section>
 			<h2>Événements inscrits</h2>
-			<table style="border:1px solid black">
-				<tr>
-					<td>Type d'événement</td>
-					<td>Date</td>
-					<td>Commentaires</td>
-					<td></td>
-				</tr>
-				<?php if($eventsInscriPost != NULL) {	//	Si il y a des events post où l'ancien est inscrit
-					foreach($eventsInscriPost as $eventInscriPost) {
-						echo '
-							<tr>
-								<td>'.$eventInscriPost->getEvenement()->getTypeEvenement()->getLibelle().'</td>
-								<td>'.$eventInscriPost->getEvenement()->getDate().'</td>
-								<td>'.$eventInscriPost->getEvenement()->getCommentaire().'</td>
-								<td><a href="evenement/'.$eventInscriPost->getEvenement()->getId().'">Voir l\'event</a></td>
-							</tr>';
-					}
-				}?>
-			</table>
+			<?php if($eventsInscriPost != NULL) { // Si il y a des events post où l'ancien est inscrit
+				echo '<ul>';
+				foreach($eventsInscriPost as $eventInscriPost) {
+					echo '<li>
+				<span class="typeEvent">'.$eventInscriPost->getEvenement()->getTypeEvenement()->getLibelle().'</span>
+				<span class="dateEvent">'.strftime('%A %d %B %Y', strtotime($eventInscriPost->getEvenement()->getDate())).'</span>
+				<p>'.$eventInscriPost->getEvenement()->getCommentaire().'</p>
+				<a href="evenement/'.$eventInscriPost->getEvenement()->getId().'">Voir l\'event</a></li>';
+				}
+				echo '</ul>';
+			} else
+				echo '<span class="sad">Aucun évènement.</span>';
+			?>
 		</section>
 		<section>
 			<h2>Autres événements</h2>
-			<ol>
-				<p>Type d'événement Date Commentaires</p>
-				<?php if($eventsInscriPost != NULL) {	//	Si il y a des events post où l'ancien n'est pas inscrit
+			<ul>
+				<?php if($eventsInscriPost != NULL) { // Si il y a des events post où l'ancien n'est pas inscrit
+					echo '<ul>';
 					foreach($eventsInscriPost as $eventInscriPost) {
 						echo '
 							<li><a href="evenement/'.$eventInscriPost->getEvenement()->getId().'">
-								'.$eventInscriPost->getEvenement()->getTypeEvenement()->getLibelle().'
-								'.$eventInscriPost->getEvenement()->getDate().'
-								'.$eventInscriPost->getEvenement()->getCommentaire().'
+								<span class="typeEvent">'.$eventInscriPost->getEvenement()->getTypeEvenement()->getLibelle().'</span>
+								<span class="dateEvent">'.strftime('%A %d %B %Y', strtotime($eventInscriPost->getEvenement()->getDate())).'</span>
+								<p>'.$eventInscriPost->getEvenement()->getCommentaire().'</p>
 							</a><a href="evenement">S\'inscrire</a></li>';
 					}
-				}?>
-			</ol>
+					echo '</ul>';
+				} else
+					echo '<span class="sad">Aucun évènement.</span>';
+				?>
+			</ul>
 		</section>
 		<section>
 			<h2>Événements passés</h2>
-			<ol>
-				<?php if ($eventsAnt != NULL) {	//	Events qui sont passés
-					foreach($eventsAnt as $eventAnt) {
-						echo '<li><a href="evenement/'.$eventAnt->getId().'"><label for="typeEvent">Type d\'événement :</label><input id="typeEvent" type="text" readonly="readonly" value="'.$eventAnt->getTypeEvenement()->getLibelle().'"/>
-						<label for="date">Date : </label><input id="date" type="text" readonly="readonly" value="'.$eventAnt->getDate().'"/>
-						<label for="commentaire">Commentaires : </label><input id="commentaire" type="text" readonly="readonly" value="'.$eventAnt->getCommentaire().'"/></a></li>';
-					}
-				}?>
-			</ol>
+			<ul>
+			<?php if ($eventsAnt != NULL) { // Events qui sont passés
+				echo '<ul>';
+				foreach($eventsAnt as $eventAnt) {
+					echo '<li><a href="evenement/'.$eventAnt->getId().'">
+						<span class="typeEvent">'.$eventAnt->getTypeEvenement()->getLibelle().'</span>
+						<span class="dateEvent">'.strftime('%A %d %B %Y', strtotime($eventAnt->getDate())).'</span>
+						<p>'.$eventAnt->getCommentaire().'</p></a></li>';
+				}
+				echo '</ul>';
+			} else
+				echo '<span class="sad">Aucun évènement.</span>';
+			?>
+			</ul>
 		</section>
-	<?php } else if ($user == "Admin") {?>
+	<?php } else if ($user == "Admin") { ?>
 		<section>
 			<h2>Événements à venir</h2>
 			<ol>
-				<?php if($eventsPost != NULL) {
-					foreach($eventsPost as $eventPost) {
-						echo '<li><label for="typeEvent">Type d\'événement :</label><input id="typeEvent" type="text" readonly="readonly" value="'.$eventPost->getTypeEvenement()->getLibelle().'"/>
-						<label for="date">Date : </label><input id="date" type="text" readonly="readonly" value="'.$eventPost->getDate().'"/>
-						<label for="commentaire">Commentaires : </label><input id="commentaire" type="text" readonly="readonly" value="'.$eventPost->getCommentaire().'"/></li>';
-					}
-				}?>
+			<?php if($eventsPost != NULL) {
+				echo '<ul>';
+				foreach($eventsPost as $eventPost) {
+					echo '<li><span class="typeEvent">'.$eventPost->getTypeEvenement()->getLibelle().'</span>
+					<span class="dateEvent">'.strftime('%A %d %B %Y', strtotime($eventPost->getDate())).'</span>
+					<p>'.$eventPost->getCommentaire().'</p></li>';
+				}
+				echo '</ul>';
+			} else
+				echo '<span class="sad">Aucun évènement.</span>';
+			?>
 			</ol>
 		</section>
 		<section>
 			<h2>Événements passés</h2>
 			<ol>
-				<?php if ($eventsAnt != NULL) {
-					foreach($eventsAnt as $eventAnt) {
-						echo '<li><label for="typeEvent">Type d\'événement :</label><input id="typeEvent" type="text" readonly="readonly" value="'.$eventAnt->getTypeEvenement()->getLibelle().'"/>
-						<label for="date">Date : </label><input id="date" type="text" readonly="readonly" value="'.$eventAnt->getDate().'"/>
-						<label for="commentaire">Commentaires : </label><input id="commentaire" type="text" readonly="readonly" value="'.$eventAnt->getCommentaire().'"/></li>';
-					}
-				}?>
+			<?php if ($eventsAnt != NULL) {
+				foreach($eventsAnt as $eventAnt) {
+					echo '<li><span class="typeEvent">'.$eventAnt->getTypeEvenement()->getLibelle().'</span>
+					<span class="dateEvent">'.strftime('%A %d %B %Y', strtotime($eventAnt->getDate())).'</span>
+					<p>'.$eventAnt->getCommentaire().'</p></li>';
+				}
+			} else
+				echo '<span class="sad">Aucun évènement.</span>';
+			?>
 			</ol>
 		</section>
 	<?php } ?>
