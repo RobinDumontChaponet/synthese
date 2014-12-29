7c780101a622ad04ee8d40eee2c938ad26da925d
RobinDumontChaponet <robin@dumontchapo.net>
2014-12-29 21:41:37 +0100

    beuargh

--- src/views/recherche.php
+++ src/views/recherche.php
@@ -1,4 +1,4 @@
-<!--meta title="Recherche" css="style/animations.css" js="script/search.js"-->
+<!--meta title="Recherche" css="style/animations.css" css="style/recherche.css" js="script/search.js"-->
 <div id="content">
 	<h1>Recherche</h1>
 	<section id="criteres">
