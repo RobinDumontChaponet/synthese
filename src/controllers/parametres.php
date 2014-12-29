e7b53722f3ca9212f9ca9576a778cf145c8baa05
lmaginot <mathieu@lalignemaginot.fr>
2014-12-29 23:46:45 +0100

    les paramètres du compte

--- src/controllers/parametres.php
+++ src/controllers/parametres.php
@@ -1,5 +1,43 @@
 <?php
 
+require_once(MODELS_INC.'NewsletterDAO.class.php');
+ $erreurs=array();
+if(isset($_POST['mod'])){
+    require_once('passwordHash.inc.php');
+    require_once(MODELS_INC.'CompteDAO.class.php');
+    if(!isset($_POST['ancienMdp']) || trim($_POST['ancienMdp'])==""){
+        $erreurs['ancien']=1;
+    }else{
+        $valid=validate_password(trim($_POST['ancienMdp']),$_SESSION['syntheseUser']->getMdp());
+        if($valid){
+            if(isset($_POST['newMdp1']) && trim($_POST['newMdp1'])!=""){
+                if(isset($_POST['newMdp2']) && trim($_POST['newMdp2'])!=""){
+                    if(trim($_POST['newMdp1'])==trim($_POST['newMdp2'])){
+
+                            $_SESSION['syntheseUser']->setMdp(create_hash(trim($_POST['newMdp1'])));
+                            CompteDAO::update($_SESSION['syntheseUser']);
+                            $ok=1;
+
+                    }else{
+                        $erreurs['corresp']=1;
+                    }
+                }else{
+                    $erreurs['mqConfirm']=1;
+                }
+            }
+            //gestion de la newsletter
+            if(isset($_POST['newsletter'])){
+                NewsletterDAO::add($_SESSION['syntheseUser']->getPersonne());
+            }else{
+                NewsletterDAO::remove($_SESSION['syntheseUser']->getPersonne());
+            }
+        }else{
+            $erreurs['ancienIncor']=1;
+        }
+    }
+
+}
+$abo=NewsletterDAO::abo($_SESSION['syntheseUser']->getPersonne());
 include(VIEWS_INC.'parametres.php');
 
 ?>
--- src/views/parametres.php
+++ src/views/parametres.php
@@ -1,4 +1,23 @@
 <!--meta title="Paramètres" css="style/animations.css"-->
 <div id="content">
-	<h1>Paramètres</h1>
+	<h1>Paramètres du compte</h1>
+    <br>
+    <form method="POST">
+        <label for="ancienMdp" >Ancien mot de passe :</label>
+        <input type="password" id="ancienMdp" name="ancienMdp" />
+        <?php if(isset($erreurs['ancienIncor'])){ echo '<b style="color:red;">Ancien Mot de passe incorrecte</b>'; }?>
+        <?php if(isset($erreurs['ancien'])){ echo '<b style="color:red;">Ancien Mot de passe obligatoire</b>'; }?><br />
+
+        <label for="newMdp1" >Nouveau mot de passe :</label>
+        <input type="password" id="newMdp1" name="newMdp1" /><br />
+        <?php if(isset($erreurs['corresp'])){ echo '<b style="color:red;">Les 2 mots de passes ne correspondent pas</b><br />'; }?>
+        <label for="newMdp2" >Nouveau mot de passe (confirmation) :</label>
+        <input type="password" id="newMdp2" name="newMdp2" />
+        <?php if(isset($erreurs['mqConfirm'])){ echo '<b style="color:red;">Confirmation obligatoire</b>'; }?><br />
+
+        <label for="newsletter" >Abonné à la newsletter :</label>
+        <input type="checkbox" name="newsletter" id="newsletter" <?php if($abo){ echo "checked"; } ?>><br />
+
+        <input type="submit" value="Modifier mon compte" name="mod">
+    </form>
 </div>
