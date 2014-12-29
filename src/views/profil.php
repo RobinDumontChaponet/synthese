d3c5b3d3bea72a1c88905efca24af8164cc14190
RobinDumontChaponet <robin@dumontchapo.net>
2014-12-29 05:52:46 +0100

    tmp

--- src/views/profil.php
+++ src/views/profil.php
@@ -16,32 +16,33 @@
 			echo '<p class="error">Mauvais format de mail</p>';
 	} ?>
 	<?php if (isset($ancien) && $ancien != NULL) {?>
+		<figure>
+			<?php if ($imageProfil != NULL)	//	Si il y a une image de profil
+				echo '<img height="230px" width="200px" src="helpers/imageProfil.php?id='.$ancien->getId().'" alt="Image de profil"/>';
+			else
+				echo '<img src="style/images/nobody.png" alt="Pas d\'image de profil"/>';
+			if ($imageTrombi != NULL)	//	Si il y a une image de trombi
+				echo '<img height="230px" width="200px" src="helpers/imageTrombi.php?id='.$ancien->getId().'" alt="Image de trombinoscope"/>';
+			else
+				echo '<img src="style/images/nobody.png" alt="Pas d\'image de trombinoscope"/>';?>
+			<!--<input type="file" name="imageProfil"/>-->
+		</figure>
 		<h1><?php echo $ancien->getPrenom()?> <?php echo strtoupper($ancien->getNomPatronymique()) ?></h1>
 		<form action="<?php ((isset($_GET['id']))?'profil':'profil/'.$_GET['id'])?>" method="post">
-			<figure>
-				<?php if ($imageProfil != NULL)	//	Si il y a une image de profil
-					echo '<img height="230px" width="200px" src="helpers/imageProfil.php?id='.$ancien->getId().'" alt="Image de profil"/>';
-				else
-					echo '<img src="style/images/nobody.png" alt="Pas d\'image de profil"/>';
-				if ($imageTrombi != NULL)	//	Si il y a une image de trombi
-					echo '<img height="230px" width="200px" src="helpers/imageTrombi.php?id='.$ancien->getId().'" alt="Image de trombinoscope"/>';
-				else
-					echo '<img src="style/images/nobody.png" alt="Pas d\'image de trombinoscope"/>';?>
-				<!--<input type="file" name="imageProfil"/>-->
-			</figure>
 			<fieldset>
 				<legend>Informations générales</legend>
 				<?php if ($_SESSION['syntheseUser']->getId() == $ancien->getId() || $_SESSION['user_auth']['write']) { // Si l'utilisateur est celui log, modif possible?>
 				<ol>
 					<li><label for="lastName">Nom d'usage :</label><input id="lastName" name="lastName" type="text" placeholder="Deuxième nom" value="<?php echo $ancien->getNom(); ?>"/></li>
-					<li><label for="sex">Sexe : </label>
+					<li><label for="sex">Sexe :</label>
 					<?php if ($_SESSION['user_auth']['write'])
 						echo '<select id="sex" name="sex"><option'.(($ancien->getSexe() == 'm')?' selected':'').' value="m">Homme</option><option'.(($ancien->getSexe() == 'f')?' selected':'').' value="f">Femme</option></select>';
 					else
 						echo '<input id="sex" type="text" readonly="readonly" value ="'.(($ancien->getSexe() == 'm')?'Homme':(($ancien->getSexe() == 'f')?'Femme':'Sexe')).'"/></li>';?>
-					
-					<li><label for="birthday">Date de naissance : </label><input id="birthday" type="text" readonly="readonly" value ="<?php echo $ancien->getDateNaissance(); ?>"/></li>
-					<li><label for="address1">Adresse :</label><input id="address1" name="address1" type="text" placeholder="Adresse" value="<?php echo $ancien->getAdresse1(); ?>"/><label for="address2">Adresse 2 :</label><input id="address2" name="address2" type="text" placeholder="Adresse 2" value="<?php echo $ancien->getAdresse2(); ?>"/></li>
+
+					<li><label for="birthday">Date de naissance :</label><input id="birthday" type="text" readonly="readonly" value ="<?php echo $ancien->getDateNaissance(); ?>"/></li>
+					<li><label for="address1">Adresse 1 :</label><input id="address1" name="address1" type="text" placeholder="Adresse" value="<?php echo $ancien->getAdresse1(); ?>"/>
+					<br /><label for="address2">Adresse 2 :</label><input id="address2" name="address2" type="text" placeholder="Adresse 2" value="<?php echo $ancien->getAdresse2(); ?>"/></li>
 					<li><label for="postalCode">Code postal :</label><input id="postalCode" name="postalCode" type="text" placeholder="Code postal" value="<?php echo $ancien->getCodePostal(); ?>"/><label for="city">Ville :</label><input id="city" name="city" type="text" placeholder="Aucune ville renseignée" value="<?php echo $ancien->getVille(); ?>"/><label for="country">Pays :</label><input id="country" name="country" type="text" placeholder = "Aucun pays renseigné" value="<?php echo $ancien->getPays(); ?>"/></li>
 					<li><label for="phoneNumber">Telephone :</label><input id="phoneNumber" name="phoneNumber" type="text" placeholder="Pas de numéro" value="<?php echo $ancien->getTelephone(); ?>"/><label for="mobileNumber">Mobile :</label><input id="mobileNumber" name="mobileNumber" type="text" placeholder="Pas de numéro" value="<?php echo $ancien->getMobile(); ?>"/></li>
 					<li><label for="mailAddress">Mail :</label><input id="mailAddress" name="mailAddress" type="text" placeholder="Pas d'adresse mail" value="<?php echo $ancien->getMail() ?>"/></li>
@@ -51,7 +52,8 @@
 					<li><label for="lastName">Nom d'usage :</label><input id="lastName" type="text" placeholder="Deuxième nom" readonly="readonly" value="<?php echo $ancien->getNom(); ?>"/></li>
 					<li><label for="sex">Sexe : </label><input id="sex" type="text" readonly="readonly" value ="<?php if ($ancien->getSexe() == "m") echo "Homme"; else if ($ancien->getSexe() == "f") echo "Femme";?>"/></li>
 					<li><label for="birthday">Date de naissance : </label><input id="birthday" type="text" readonly="readonly" value ="<?php echo $ancien->getDateNaissance(); ?>"/></li>
-					<li><label for="address1">Adresse :</label><input id="address1" type="text" placeholder="Adresse" readonly="readonly" value="<?php echo $ancien->getAdresse1(); ?>"/><label for="address2">Adresse 2 :</label><input id="address2" type="text" placeholder="Adresse 2" readonly="readonly" value="<?php echo $ancien->getAdresse2(); ?>"/></li>
+					<li><label for="address1">Adresse 1 :</label><input id="address1" type="text" placeholder="Adresse" readonly="readonly" value="<?php echo $ancien->getAdresse1(); ?>"/>
+					<br /><label for="address2">Adresse 2 :</label><input id="address2" type="text" placeholder="Adresse 2" readonly="readonly" value="<?php echo $ancien->getAdresse2(); ?>"/></li>
 					<li><label for="postalCode">Code postal :</label><input id="postalCode" type="text" placeholder="Code postal" readonly="readonly" value="<?php echo $ancien->getCodePostal(); ?>"/><label for="city">Ville :</label><input id="city" type="text" placeholder="Aucune ville renseignée" readonly="readonly" value="<?php echo $ancien->getVille(); ?>"/><label for="country">Pays :</label><input id="country" type="text" placeholder = "Aucun pays renseigné" readonly="readonly" value="<?php echo $ancien->getPays(); ?>"/></li>
 					<li><label for="phoneNumber">Telephone :</label><input id="phoneNumber" type="text" placeholder="Pas de numéro" readonly="readonly" value="<?php echo $ancien->getTelephone(); ?>"/><label for="mobileNumber">Mobile :</label><input id="mobileNumber" type="text" placeholder="Pas de numéro" readonly="readonly" value="<?php echo $ancien->getMobile(); ?>"/></li>
 					<li><label for="mailAddress">Mail :</label><input id="mailAddress" type="text" placeholder="Pas d'adresse mail" readonly="readonly" value=""/></li>
@@ -63,24 +65,24 @@
 				<ol>
 					<?php if ($diplomeDUT != NULL) { // Ne peut être modifié, fixe et normalement présent?>
 					<li>
-						<label for="diplomeDUT">Diplôme : </label>
+						<label for="diplomeDUT">Diplôme :</label>
 						<input id="diplomeDUT" type="text" placeholder="Diplome" readonly="readonly" value="<?php echo $diplomeDUT->getDiplomeDUT()->getLibelle();?>"/>
-						<label for="departement">Département : </label>
+						<label for="departement">Département :</label>
 						<input id="departement" type="text" placeholder="Département" readonly="readonly" value="<?php echo $diplomeDUT->getDepartementIUT()->getNom();?>"/>
-						<label for="promotion">Promotion : </label>
+						<label for="promotion">Promotion :</label>
 						<input id="promotion" type="text" placeholder="Promotion" readonly="readonly" value="<?php echo $diplomeDUT->getPromotion()->getAnnee();?>"/>
 					</li>
 					<?php }
 					if ($diplomesPost != NULL) { // Il faudra faire quelque chose pour pouvoir les modifiers, soit là, soit sur une autre page
 						foreach($diplomesPost as $diplomePost) {?>
 							<li>
-								<a href="diplome/<?php echo $diplomePost->getDiplomePostDUT()->getId();?>"><label for="diplomePost<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Diplôme : </label>
+								<a href="diplome/<?php echo $diplomePost->getDiplomePostDUT()->getId();?>"><label for="diplomePost<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Diplôme :</label>
 								<input id="diplomePost<?php echo $diplomePost->getDiplomePostDUT()->getId();?>" type="text" placeholder="Diplome" readonly="readonly" value="<?php echo $diplomePost->getDiplomePostDUT()->getLibelle();?>"/></a>
-								<a href="etablissement/<?php echo $diplomePost->getEtablissement()->getId();?>"><label for="etablissement<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Établissement : </label>
+								<a href="etablissement/<?php echo $diplomePost->getEtablissement()->getId();?>"><label for="etablissement<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Établissement :</label>
 								<input id="etablissement<?php echo $diplomePost->getDiplomePostDUT()->getId();?>" type="text" placeholder="Établissement" readonly="readonly" value="<?php echo $diplomePost->getEtablissement()->getNom();?>"/></a>
-								<label for="resultat<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Résultat : </label>
+								<label for="resultat<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Résultat :</label>
 								<input id="resultat<?php echo $diplomePost->getDiplomePostDUT()->getId();?>" type="text" placeholder="Résultat" readonly="readonly" value="<?php echo $diplomePost->getResultat();?>"/>
-								<label for="periode<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Période : </label>
+								<label for="periode<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Période :</label>
 								<input id="periode<?php echo $diplomePost->getDiplomePostDUT()->getId();?>" type="text" placeholder="Résultat" readonly="readonly" value="<?php echo substr($diplomePost->getDateDebut(), 0, 4);?> - <?php echo substr($diplomePost->getDateFin(), 0, 4);?>"/>
 								<?php if ($_SESSION[syntheseUser]->getId() == $ancien->getId() || $_SESSION['user_auth']['write']) {?><aside><a href="modif">Modifier (ou faire un lien sur la ligne d'info)</a><a href="suppr">Supprimer</a></aside><?php }?>
 							</li>
@@ -97,11 +99,11 @@
 					<?php if($entreprises != NULL) { // Il faudra faire quelque chose pour pouvoir les modifiers, soit là, soit sur une autre page
 						foreach($entreprises as $entreprise) {?>
 							<li>
-								<a href="entreprise/<?php echo $entreprise->getEntreprise()->getId()?>"><label for="entreprise<?php echo $entreprise->getEntreprise()->getId()?>">Entreprise : </label>
+								<a href="entreprise/<?php echo $entreprise->getEntreprise()->getId()?>"><label for="entreprise<?php echo $entreprise->getEntreprise()->getId()?>">Entreprise :</label>
 								<input id="entreprise<?php echo $entreprise->getEntreprise()->getId()?>" type="text" placeholder="Entreprise" readonly="readonly" value="<?php echo $entreprise->getEntreprise()->getNom();?>"/></a>
-								<label for="poste<?php echo $entreprise->getEntreprise()->getId()?>">Poste : </label>
+								<label for="poste<?php echo $entreprise->getEntreprise()->getId()?>">Poste :</label>
 								<input id="poste<?php echo $entreprise->getEntreprise()->getId()?>" type="text" placeholder="Poste" readonly="readonly" value="<?php echo $entreprise->getPoste()->getLibelle();?>"/>
-								<label for="periode<?php echo $entreprise->getEntreprise()->getId()?>">Période : </label>
+								<label for="periode<?php echo $entreprise->getEntreprise()->getId()?>">Période :</label>
 								<input id="periode<?php echo $entreprise->getEntreprise()->getId()?>" type="text" placeholder="Période" readonly="readonly" value="<?php echo $entreprise->getDateEmbaucheDeb()?> à <?php if($entreprise->getDateEmbaucheFin() == NULL) echo 'maintenant'; else echo $entreprise->getDateEmbaucheFin()?>"/>
 							</li>
 						<?php }
