cd2685650585ddeb03ff195709f7287f5410780a
JozwickiVictor <tigre.demon@hotmail.fr>
2014-12-30 14:03:48 +0100

    events & diplômes

--- src/views/diplome.php
+++ src/views/diplome.php
@@ -1,11 +1,17 @@
 <!--meta title="<?php echo (($diplome != NULL)?'Diplôme : '.$diplome->getLibelle():'Diplôme non trouvé'); ?>" css="style/animations.css"-->
 <div id="content">
 	<?php if ($diplome != NULL) { ?>
-		<ul>
-			<li><label for="diplomeLibelle">Libelle du diplôme</label><input id="diplomeLibelle" name="diplomeLibelle" type="text" placeholder="Libelle du diplôme" value="<?php echo $diplome->getLibelle(); ?>"/></li>
-			<li><label for="domainLibelle">Domaine</label><input id="domainLibelle" name="domainLibelle" type="text" placeholder="Domaine" value="<?php echo $diplome->getDomaine()->getLibelle(); ?>"/></li>
-			<li><label for="domainDescription">Description</label><input id="domainDescription" name="domainDescription" type="text" placeholder="Description" value="<?php echo $diplome->getDomaine()->getDescription(); ?>"/></li>
-		</ul>
+		<section>
+			<h2>Détails du diplôme</h2>
+			<dl>
+				<dt id="diplomeLibelle">Libelle du diplôme</dt>
+				<dd><?php echo $diplome->getLibelle(); ?></li>
+				<dt id="domainLibelle">Domaine</dt>
+				<dd><?php echo $diplome->getDomaine()->getLibelle(); ?></dd>
+				<dt id="domainDescription">Description</dt>
+				<dd><?php echo $diplome->getDomaine()->getDescription(); ?></dd>
+			</ul>
+		</section>
 		<?php if ($_SESSION['user_auth']['write'])
 			echo '<a href="diplome-editer/'.$_GET['id'].'">Editer le diplôme</a>';
 	} else {?>
--- src/views/evenement.php
+++ src/views/evenement.php
@@ -1,5 +1,21 @@
 <!--meta title="Événement" css="style/animations.css"-->
 
 <div id="content">
-	Page de l'évent : <?php echo $_GET['id'];?>
+	<?php if ($event != NULL) { ?>
+		<section>
+			<h2>Détails de l'événement</h2>
+			<dl>
+				<dt id="eventLibelle">Type d'événement</dt>
+				<dd><?php echo $event->getTypeEvenement()->getLibelle();?></dd>
+				<dt id="date">Date </dt>
+				<dd><?php echo $event->getDate(); ?></dd>
+				<dt id="comment">Commentaire</dt>
+				<dd><?php echo $event->getCommentaire(); ?></dd>
+			</dl>
+		</section>
+		<?php if ($_SESSION['user_auth']['write'])
+			echo '<a href="evenement-editer/'.$_GET['id'].'">Editer l\'événement</a>';
+	} else {?>
+		<p class="warning">Cet événement n'existe pas</p>
+	<?php }?>
 </div>
