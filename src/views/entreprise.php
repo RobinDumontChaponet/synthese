0cc6c5a8da52ce141a0372a4b97d3b4910780ca9
JozwickiVictor <tigre.demon@hotmail.fr>
2014-12-30 14:19:48 +0100

    entreprise

--- src/views/entreprise.php
+++ src/views/entreprise.php
@@ -1,25 +1,22 @@
 <!--meta title="<?php echo (($entreprise != NULL)?$entreprise->getNom():'Entreprise non trouvé'); ?>" css="style/animations.css"-->
 <div id="content">
-	<?php if ($entreprise != NULL) {
-			if ($libelleTypeProfil == "Ancien") { /* Ne peut pas modifier */ ?>
-			<ol>
-				<li><label for="companyName">Nom de l'entreprise</label><input id="companyName" type="text" placeholder="Nom de l'entreprise" readonly="readonly" value="<?php echo $entreprise->getNom(); ?>"/></li>
-				<li><label for="address1">Adresse 1</label><input id="address1" type="text" placeholder="Adresse 1" readonly="readonly" value="<?php echo $entreprise->getAdresse1(); ?>"/></li>
-				<li><label for="address2">Adresse 2</label><input id="address2" type="text" placeholder="Adresse 2" readonly="readonly" value="<?php echo $entreprise->getAdresse2(); ?>"/></li>
-				<li><label for="postalCode">Code postal</label><input id="postalCode" type="text" placeholder="Code postal" readonly="readonly" value="<?php echo $entreprise->getCodePostal(); ?>"/></li>
-				<li><label for="city">Ville</label><input id="city" type="text" placeholder="Ville" readonly="readonly" value="<?php echo $entreprise->getVille(); ?>"/></li>
-				<li><label for="country">Pays</label><input id="country" type="text" placeholder="Pays" readonly="readonly" value="<?php echo $entreprise->getPays(); ?>"/></li>
-			</ol>
-		<?php } else if ($libelleTypeProfil == "Admin" || $libelleTypeProfil == "Professeur") { /*Peut modifier*/ ?>
-			<ol>
-				<li><label for="companyName">Nom de l'entreprise :</label><input id="companyName" type="text" placeholder="Nom de l'entreprise" value="<?php echo $entreprise->getNom(); ?>"/></li>
-				<li><label for="address1">Adresse 1</label><input id="address1" type="text" placeholder="Adresse 1" value="<?php echo $entreprise->getAdresse1(); ?>"/></li>
-				<li><label for="address2">Adresse 2</label><input id="address2" type="text" placeholder="Adresse 2" value="<?php echo $entreprise->getAdresse2(); ?>"/></li>
-				<li><label for="postalCode">Code postal</label><input id="postalCode" type="text" placeholder="Code postal" value="<?php echo $entreprise->getCodePostal(); ?>"/></li>
-				<li><label for="city">Ville</label><input id="city" type="text" placeholder="Ville" value="<?php echo $entreprise->getVille(); ?>"/></li>
-				<li><label for="country">Pays</label><input id="country" type="text" placeholder="Pays" value="<?php echo $entreprise->getPays(); ?>"/></li>
-			</ol>
-		<?php }
+	<?php if ($entreprise != NULL) { ?>
+		<dl>
+			<dt id="companyName">Nom de l'entreprise</dt>
+			<dd><?php echo $entreprise->getNom(); ?></dd>
+			<dt id="address1">Adresse 1</label>
+			<dd><?php echo $entreprise->getAdresse1(); ?></dd>
+			<dt id="address2">Adresse 2</dt>
+			<dd><?php echo $entreprise->getAdresse2(); ?></dd>
+			<dt id="postalCode">Code postal</label>
+			<dd><?php echo $entreprise->getCodePostal(); ?></dd>
+			<dt id="city">Ville</dt>
+			<dd><?php echo $entreprise->getVille(); ?></dd>
+			<dt id="country">Pays</dt>
+			<dd><?php echo $entreprise->getPays(); ?></dd>
+		</dl>
+		<?php if ($_SESSION['user_auth']['write'])
+			echo '<a href="entreprise-editer/'.$_GET['id'].'">Editer l\'entreprise</a>';
 	} else { ?>
 		<p class="warning">Cette entreprise n'existe pas</p>
 	<?php } ?>
