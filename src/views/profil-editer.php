7511c2a39a19cbbfb27be3bcdbd561d8bb55dff9
JozwickiVictor <tigre.demon@hotmail.fr>
2014-12-30 02:11:14 +0100

    kirei desu

--- src/views/profil-editer.php
+++ src/views/profil-editer.php
@@ -1,117 +1,141 @@
 <!--meta title="<?php if ($ancien != NULL){echo 'Modification profil de '.$ancien->getNomPatronymique().$ancien->getPrenom();} else {echo 'Profil non trouvé';}?>" css="style/animations.css" css="style/profil.css"-->
-<section id="content">
-	<?php 
-	if ($valid) {
-		if (isset($valid['lastName']) && !$valid['lastName'])
-			echo '<p class="error">Le nom doit être écrit en lettres</p>';
-		if (isset($valid['lastName']) && !$valid['lastName'])
-			echo '<p class="error">Le nom patronymique doit être écrit en lettres</p>';
-		if (isset($valid['firstName']) && !$valid['firstName'])
-			echo '<p class="error">Le prénom doit être écrit en lettres</p>';
-		if  (isset($valid['city']) && !$valid['city'])
-			echo '<p class="error">La ville doit être écrit en lettres</p>';
-		if (isset($valid['country']) && !$valid['country'])
-			echo '<p class="error">Le pays doit être écrit en lettres</p>';
-		if  (isset($valid['phoneNumber']) && !$valid['phoneNumber'])
-			echo '<p class="error">Mauvais format de téléphone</p>';
-		if (isset($valid['mobileNumber']) && !$valid['mobileNumber'])
-			echo '<p class="error">Mauvais format de mobile</p>';
-		if (isset($valid['mailAddress']) && !$valid['mailAddress'])
-			echo '<p class="error">Mauvais format de mail</p>';
-		if (isset($valid['birthday']) && !$valid['birthday'])
-			echo '<p class="error">Mauvais format de date : YYYY-MM-DD</p>';
-	}
+<div id="content">
+<?php 
+if ($valid) {
+	if (isset($valid['lastName']) && !$valid['lastName'])
+		echo '<p class="error">Le nom doit être écrit en lettres</p>';
+	if (isset($valid['lastName']) && !$valid['lastName'])
+		echo '<p class="error">Le nom patronymique doit être écrit en lettres</p>';
+	if (isset($valid['firstName']) && !$valid['firstName'])
+		echo '<p class="error">Le prénom doit être écrit en lettres</p>';
+	if  (isset($valid['city']) && !$valid['city'])
+		echo '<p class="error">La ville doit être écrit en lettres</p>';
+	if (isset($valid['country']) && !$valid['country'])
+		echo '<p class="error">Le pays doit être écrit en lettres</p>';
+	if  (isset($valid['phoneNumber']) && !$valid['phoneNumber'])
+		echo '<p class="error">Mauvais format de téléphone</p>';
+	if (isset($valid['mobileNumber']) && !$valid['mobileNumber'])
+		echo '<p class="error">Mauvais format de mobile</p>';
+	if (isset($valid['mailAddress']) && !$valid['mailAddress'])
+		echo '<p class="error">Mauvais format de mail</p>';
+	if (isset($valid['birthday']) && !$valid['birthday'])
+		echo '<p class="error">Mauvais format de date : YYYY-MM-DD</p>';
+}
 	
-	if (isset($ancien) && $ancien != NULL && ($_SESSION['syntheseUser']->getId() == $ancien->getId() || $_SESSION['user_auth']['write'])) {?>
-		<figure>
-			<?php if ($imageProfil != NULL)	//	Si il y a une image de profil
-				echo '<img height="230px" width="200px" src="helpers/imageProfil.php?id='.$ancien->getId().'" alt="Image de profil"/>';
-			else
-				echo '<img src="style/images/nobody.png" alt="Pas d\'image de profil"/>';
-			if ($imageTrombi != NULL)	//	Si il y a une image de trombi
-				echo '<img height="230px" width="200px" src="helpers/imageTrombi.php?id='.$ancien->getId().'" alt="Image de trombinoscope"/>';
-			else
-				echo '<img src="style/images/nobody.png" alt="Pas d\'image de trombinoscope"/>';?>
-			<!--<input type="file" name="imageProfil"/> Il faut faire un input sur cette page pour upload/supprimer l'image de profil vu que tu as dit que tu avais déjà des trucs tout bien fait et tout et que tu t'en occuperais alors j'ai laissé ça comme ça et j'espère que le commentaire est assez grand pour que tu le vois!-->
-		</figure>
-		
-		<form action="<?php ((isset($_GET['id']))?'profil':'profil/'.$_GET['id'])?>" method="post">
-		<?php if ($_SESSION['user_auth']['write'])
-			echo '<input type="text" placeholder="Prénom" name="firstName" value="'.$ancien->getPrenom().'"></input><input type="text" placeholder="Nom" name="name" value="'.$ancien->getNomPatronymique().'"></input>';
+if (isset($ancien) && $ancien != NULL && ($_SESSION['syntheseUser']->getId() == $ancien->getId() || $_SESSION['user_auth']['write'])) {?>
+	<figure>
+		<?php if ($imageProfil != NULL)	//	Si il y a une image de profil
+			echo '<img height="230px" width="200px" src="helpers/imageProfil.php?id='.$ancien->getId().'" alt="Image de profil"/>';
 		else
-			echo '<input type="text" placeholder="Prénom" readonly="readonly" value="'.$ancien->getPrenom().'"></input><input type="text" placeholder="Nom" readonly="readonly" value="'.$ancien->getNomPatronymique().'"></input>'; ?>
-			<fieldset>
-				<legend>Informations générales</legend>
-				<ol>
-					<li><label for="lastName">Nom d'usage :</label><input id="lastName" name="lastName" type="text" placeholder="Deuxième nom" value="<?php echo $ancien->getNom(); ?>"/></li>
-					<li><label for="sex">Sexe :</label>
-					<?php if ($_SESSION['user_auth']['write'])
-						echo '<select id="sex" name="sex"><option'.(($ancien->getSexe() == 'm')?' selected':'').' value="m">Homme</option><option'.(($ancien->getSexe() == 'f')?' selected':'').' value="f">Femme</option></select>';
-					else
-						echo '<input id="sex" type="text" readonly="readonly" value ="'.(($ancien->getSexe() == 'm')?'Homme':(($ancien->getSexe() == 'f')?'Femme':'Sexe')).'"/></li>';?>
-					<li><label for="birthday">Date de naissance :</label><?php if ($_SESSION['user_auth']['write'])
-						echo '<input id="birthday" name="birthday" type="text" value ="'.$ancien->getDateNaissance().'"/>';
-					else
-						echo '<input id="birthday" type="text" readonly="readonly" value ="'.$ancien->getDateNaissance().'"/></li>';?>
-					<li><label for="address1">Adresse 1 :</label><input id="address1" name="address1" type="text" placeholder="Adresse" value="<?php echo $ancien->getAdresse1(); ?>"/>
-					<br /><label for="address2">Adresse 2 :</label><input id="address2" name="address2" type="text" placeholder="Adresse 2" value="<?php echo $ancien->getAdresse2(); ?>"/></li>
-					<li><label for="postalCode">Code postal :</label><input id="postalCode" name="postalCode" type="text" placeholder="Code postal" value="<?php echo $ancien->getCodePostal(); ?>"/><label for="city">Ville :</label><input id="city" name="city" type="text" placeholder="Aucune ville renseignée" value="<?php echo $ancien->getVille(); ?>"/><label for="country">Pays :</label><input id="country" name="country" type="text" placeholder = "Aucun pays renseigné" value="<?php echo $ancien->getPays(); ?>"/></li>
-					<li><label for="phoneNumber">Telephone :</label><input id="phoneNumber" name="phoneNumber" type="text" placeholder="Pas de numéro" value="<?php echo $ancien->getTelephone(); ?>"/><label for="mobileNumber">Mobile :</label><input id="mobileNumber" name="mobileNumber" type="text" placeholder="Pas de numéro" value="<?php echo $ancien->getMobile(); ?>"/></li>
-					<li><label for="mailAddress">Mail :</label><input id="mailAddress" name="mailAddress" type="text" placeholder="Aucune adresse mail" value="<?php echo $ancien->getMail() ?>"/></li>
-				</ol>
-			</fieldset>
-			<fieldset>
-				<legend>Diplômes</legend>
-				<ol>
-					<?php if ($diplomeDUT != NULL) { // Ne peut être modifié, fixe et normalement présent?>
+			echo '<img src="style/images/nobody.png" alt="Pas d\'image de profil"/>';
+		if ($imageTrombi != NULL)	//	Si il y a une image de trombi
+			echo '<img height="230px" width="200px" src="helpers/imageTrombi.php?id='.$ancien->getId().'" alt="Image de trombinoscope"/>';
+		else
+			echo '<img src="style/images/nobody.png" alt="Pas d\'image de trombinoscope"/>';?>
+		<!--<input type="file" name="imageProfil"/> Il faut faire un input sur cette page pour upload/supprimer l'image de profil vu que tu as dit que tu avais déjà des trucs tout bien fait et tout et que tu t'en occuperais alors j'ai laissé ça comme ça et j'espère que le commentaire est assez grand pour que tu le vois!-->
+	</figure>
+	<form action="<?php ((isset($_GET['id']))?'profil':'profil/'.$_GET['id'])?>" method="post">
+		<?php if ($_SESSION['user_auth']['write'])
+			echo '<h1><input type="text" placeholder="Prénom" name="firstName" value="'.$ancien->getPrenom().'"></input><input type="text" placeholder="Nom" name="name" value="'.$ancien->getNomPatronymique().'"></input></h1>';
+		else
+			echo '<h1>'.$ancien->getPrenom().'<span id="nomPatronymique"> '.$ancien->getNomPatronymique().'</span></h1>'; ?>
+		<section>
+			<h2>Informations générales</h2>
+			<dl>
+				<dt></label for="lastName">Nom d'usage</label></dt>
+				<dd><input id="lastName" name="lastName" type="text" placeholder="Deuxième nom" value="<?php echo $ancien->getNom();?>"/></dd>
+				<dt></label for="sex">Sexe</label></dt>
+				<?php if ($_SESSION['user_auth']['write'])	//	L'admin a tout les droits
+					echo '<dd><select id="sex" name="sex"><option'.(($ancien->getSexe() == 'm')?' selected':'').' value="m">Homme</option><option'.(($ancien->getSexe() == 'f')?' selected':'').' value="f">Femme</option></select></dd>';
+				else
+					echo '<dd><input id="sex" type="text" readonly="readonly" value ="'.(($ancien->getSexe() == 'm')?'Homme':(($ancien->getSexe() == 'f')?'Femme':'Sexe')).'"/></dd>';?>
+				<dt></label for="birthday">Date de naissance</label></dt>
+				<?php if ($_SESSION['user_auth']['write'])	//	L'admin a tout les droits
+					echo '<dd><input id="birthday" name="birthday" type="text" value ="'.$ancien->getDateNaissance().'"/></dd>';
+				else
+					echo '<dd><input id="birthday" type="text" readonly="readonly" value ="'.$ancien->getDateNaissance().'"/></dd>';?>
+				<dt></label for="address1">Adresse 1</label></dt>
+				<dd><input id="address1" name="address1" type="text" placeholder="Adresse" value="<?php echo $ancien->getAdresse1();?>"/></dd>
+				<dt></label for="address2">Adresse 2</label></dt>
+				<dd><input id="address2" name="address2" type="text" placeholder="Adresse 2" value="<?php echo $ancien->getAdresse2(); ?>"/></dd>
+				<dt></label for="postalCode">Code postal</label></dt>
+				<dd><input id="postalCode" name="postalCode" type="text" placeholder="Code postal" value="<?php echo $ancien->getCodePostal(); ?>"/></dd>
+				<dt></label for="city">Ville :</label></dt>
+				<dd><input id="city" name="city" type="text" placeholder="Aucune ville renseignée" value="<?php echo $ancien->getVille(); ?>"/></dd>
+				<dt></label for="country">Pays :</label></dt>
+				<dd><input id="country" name="country" type="text" placeholder = "Aucun pays renseigné" value="<?php echo $ancien->getPays(); ?>"/></li></dd>
+				<dt></label for="phoneNumber">Telephone</label></dt>
+				<dd><input id="phoneNumber" name="phoneNumber" type="text" placeholder="Pas de numéro" value="<?php echo $ancien->getTelephone(); ?>"/></dd>
+				<dt></label for="mobileNumber">Mobile :</label></dt>
+				<dd><input id="mobileNumber" name="mobileNumber" type="text" placeholder="Pas de numéro" value="<?php echo $ancien->getMobile(); ?>"/></dd>
+				<dt></label for="mailAddress">Mail</label></dt>
+				<dd><input id="mailAddress" name="mailAddress" type="text" placeholder="Aucune adresse mail" value="<?php echo $ancien->getMail() ?>"/></dd>
+			</dl>
+		</section>
+		<section>
+			<h2>Diplômes</h2>
+			<ul>
+				<?php if ($diplomeDUT != NULL) { // Ne peut être modifié, fixe et normalement présent?>
 					<li>
-						<label for="diplomeDUT">Diplôme :</label>
-						<input id="diplomeDUT" type="text" placeholder="Diplome" readonly="readonly" value="<?php echo $diplomeDUT->getDiplomeDUT()->getLibelle();?>"/>
-						<label for="departement">Département :</label>
-						<input id="departement" type="text" placeholder="Département" readonly="readonly" value="<?php echo $diplomeDUT->getDepartementIUT()->getNom();?>"/>
-						<label for="promotion">Promotion :</label>
-						<input id="promotion" type="text" placeholder="Promotion" readonly="readonly" value="<?php echo $diplomeDUT->getPromotion()->getAnnee();?>"/>
+						<?php if ($_SESSION['user_auth']['write'])	//	En cas où le diplôme n'est pas le bon
+							echo '<a href="#">Modifier le diplôme de cet élève</a>'; ?>
+						<h3><?php echo $diplomeDUT->getDiplomeDUT()->getLibelle();?></h3>
+						<dl>
+							<dt></label for="departement">Département</label></dt>
+							<dd><?php echo $diplomeDUT->getDepartementIUT()->getNom();?></dd>
+							<dt></label for="promotion">Promotion</label></dt>
+							<dd><?php echo $diplomeDUT->getPromotion()->getAnnee();?></dd>
+						</dl>
 					</li>
+				<?php }
+				if ($diplomesPost != NULL) { // Il faudra faire quelque chose pour pouvoir les modifiers, soit là, soit sur une autre page
+					foreach($diplomesPost as $diplomePost) {?>
+						<li>
+							<h3><a href="diplome/<?php echo $diplomePost->getDiplomePostDUT()->getId();?>"><?php echo $diplomePost->getDiplomePostDUT()->getLibelle();?></a></h3>
+							<dl>
+								<dt>Établissement</dt>
+								<dd><a href="etablissement/<?php echo $diplomePost->getEtablissement()->getId();?>"><?php echo $diplomePost->getEtablissement()->getNom();?></a></dd>
+								<dt>Résultat</dt>
+								<dd><?php echo $diplomePost->getResultat();?></dd>
+								<dt>Période</dt>
+								<dd><?php echo substr($diplomePost->getDateDebut(), 0, 4);?> - <?php echo substr($diplomePost->getDateFin(), 0, 4);?></dd>
+							</dl>
+							<a href="modif">Modifier</a>
+							<a href="diplome-supprimer">Supprimer</a>
+						</li>
 					<?php }
-					if ($diplomesPost != NULL) { // Il faudra faire quelque chose pour pouvoir les modifiers, soit là, soit sur une autre page
-						foreach($diplomesPost as $diplomePost) {?>
-							<li>
-								<a href="diplome/<?php echo $diplomePost->getDiplomePostDUT()->getId();?>"><label for="diplomePost<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Diplôme :</label>
-								<input id="diplomePost<?php echo $diplomePost->getDiplomePostDUT()->getId();?>" type="text" placeholder="Diplome" readonly="readonly" value="<?php echo $diplomePost->getDiplomePostDUT()->getLibelle();?>"/></a>
-								<a href="etablissement/<?php echo $diplomePost->getEtablissement()->getId();?>"><label for="etablissement<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Établissement :</label>
-								<input id="etablissement<?php echo $diplomePost->getDiplomePostDUT()->getId();?>" type="text" placeholder="Établissement" readonly="readonly" value="<?php echo $diplomePost->getEtablissement()->getNom();?>"/></a>
-								<label for="resultat<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Résultat :</label>
-								<input id="resultat<?php echo $diplomePost->getDiplomePostDUT()->getId();?>" type="text" placeholder="Résultat" readonly="readonly" value="<?php echo $diplomePost->getResultat();?>"/>
-								<label for="periode<?php echo $diplomePost->getDiplomePostDUT()->getId();?>">Période :</label>
-								<input id="periode<?php echo $diplomePost->getDiplomePostDUT()->getId();?>" type="text" placeholder="Résultat" readonly="readonly" value="<?php echo substr($diplomePost->getDateDebut(), 0, 4);?> - <?php echo substr($diplomePost->getDateFin(), 0, 4);?>"/>
-								<aside><a href="modif">Modifier (ou faire un lien sur la ligne d'info)</a><a href="suppr">Supprimer</a></aside>
-							</li>
-						<?php }
-					} ?>
-						<li><aside><a href="ajouter">Ajouter +</a></aside></li>
-				</ol>
-			</fieldset>
-			<fieldset>
-				<legend>Entreprises</legend>
-				<ol>
-					<?php if($entreprises != NULL) { // Il faudra faire quelque chose pour pouvoir les modifiers, soit là, soit sur une autre page
-						foreach($entreprises as $entreprise) {?>
-							<li>
-								<a href="entreprise/<?php echo $entreprise->getEntreprise()->getId()?>"><label for="entreprise<?php echo $entreprise->getEntreprise()->getId()?>">Entreprise :</label>
-								<input id="entreprise<?php echo $entreprise->getEntreprise()->getId()?>" type="text" placeholder="Entreprise" readonly="readonly" value="<?php echo $entreprise->getEntreprise()->getNom();?>"/></a>
-								<label for="poste<?php echo $entreprise->getEntreprise()->getId()?>">Poste :</label>
-								<input id="poste<?php echo $entreprise->getEntreprise()->getId()?>" type="text" placeholder="Poste" readonly="readonly" value="<?php echo $entreprise->getPoste()->getLibelle();?>"/>
-								<label for="periode<?php echo $entreprise->getEntreprise()->getId()?>">Période :</label>
-								<input id="periode<?php echo $entreprise->getEntreprise()->getId()?>" type="text" placeholder="Période" readonly="readonly" value="<?php echo $entreprise->getDateEmbaucheDeb()?> à <?php if($entreprise->getDateEmbaucheFin() == NULL) echo 'maintenant'; else echo $entreprise->getDateEmbaucheFin()?>"/>
-							</li>
-						<?php }
-					} ?>
-					<li><aside><a href="ajouter">Ajouter +</a><a href="suppr">Supprimer</a></aside></li>
-				</ol>
-			</fieldset>
-			<input type="submit" value="Enregistrer les modifications" />
-		</form>
+				} ?>
+				<li>
+					<a href="diplome-ajouter/<?php echo $ancien->getId();?>">Ajouter un nouveau diplôme</a>
+				</li>
+			</ul>
+		</section>
+		<section>
+			<h2>Entreprises</h2>
+			<ul>
+				<?php if($entreprises != NULL) { // Il faudra faire quelque chose pour pouvoir les modifiers, soit là, soit sur une autre page
+					foreach($entreprises as $entreprise) {?>
+						<li>
+							<h3><a href="entreprise/<?php echo $entreprise->getEntreprise()->getId()?>"><?php echo $entreprise->getEntreprise()->getNom();?></a></h3>
+							<dl>
+								<dt>Poste</dt>
+								<dd><?php echo $entreprise->getPoste()->getLibelle();?></dd>
+								<dt>Période</dt>
+								<dd><?php echo $entreprise->getDateEmbaucheDeb()?> à <?php if($entreprise->getDateEmbaucheFin() == NULL) echo 'maintenant'; else echo $entreprise->getDateEmbaucheFin()?></dd>
+							</dl>
+							<a href="modif">Modifier</a>
+							<a href="entreprise-supprimer/<?php echo $entreprise->getEntreprise()->getId(); ?>">Supprimer</a>
+						</li>
+					<?php }
+				} ?>
+				<li>
+					<a href="entreprise-ajouter/<?php echo $ancien->getId();?>">Ajouter une nouvelle entreprise</a>
+				</li>
+			</ul>
+		</section>
+		<input type="submit" value="Enregistrer les modifications" />
+	</form>
 	<?php } else {?>
 		<p class="warning">Vous ne pouvez pas modifier ce profil</p>
 	<?php }?>
-</section>
+</div>
