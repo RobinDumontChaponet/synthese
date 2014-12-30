9558a928ca9cd51b9116950698d3eb4975a0e742
RobinDumontChaponet <robin@dumontchapo.net>
2014-12-30 08:21:16 +0100

    Ils m'avaient échappés, ceux là ;-) Je vais me coucher

--- src/views/diplome.php
+++ src/views/diplome.php
@@ -2,9 +2,9 @@
 <div id="content">
 	<?php if ($diplome != NULL) { ?>
 		<ul>
-			<li><label for="diplomeLibelle">Libelle du diplôme :</label><input id="diplomeLibelle" name="diplomeLibelle" type="text" placeholder="Libelle du diplôme" value="<?php echo $diplome->getLibelle(); ?>"/></li>
-			<li><label for="domainLibelle">Domaine :</label><input id="domainLibelle" name="domainLibelle" type="text" placeholder="Domaine" value="<?php echo $diplome->getDomaine()->getLibelle(); ?>"/></li>
-			<li><label for="domainDescription">Description :</label><input id="domainDescription" name="domainDescription" type="text" placeholder="Description" value="<?php echo $diplome->getDomaine()->getDescription(); ?>"/></li>
+			<li><label for="diplomeLibelle">Libelle du diplôme</label><input id="diplomeLibelle" name="diplomeLibelle" type="text" placeholder="Libelle du diplôme" value="<?php echo $diplome->getLibelle(); ?>"/></li>
+			<li><label for="domainLibelle">Domaine</label><input id="domainLibelle" name="domainLibelle" type="text" placeholder="Domaine" value="<?php echo $diplome->getDomaine()->getLibelle(); ?>"/></li>
+			<li><label for="domainDescription">Description</label><input id="domainDescription" name="domainDescription" type="text" placeholder="Description" value="<?php echo $diplome->getDomaine()->getDescription(); ?>"/></li>
 		</ul>
 		<?php if ($_SESSION['user_auth']['write'])
 			echo '<a href="diplome-editer/'.$_GET['id'].'">Editer le diplôme</a>';
