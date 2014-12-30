a15627bd34217ba8fda329936593c7b1ec4e3f18
RobinDumontChaponet <robin@dumontchapo.net>
2014-12-30 19:30:26 +0100

    style

--- src/views/evenement.php
+++ src/views/evenement.php
@@ -1,16 +1,18 @@
 <!--meta title="Événement" css="style/animations.css" css="style/evenements.css"-->
 <div id="content">
-	<?php if ($event != NULL) { ?>
-		<h1>Détails de l'événement</h1>
+<?php if ($event != NULL) { ?>
+		<?php if ($_SESSION['user_auth']['write'])
+			echo '<a class="aEdit" href="evenement-editer/'.$_GET['id'].'">Editer l\'événement</a><a class="aEdit" href="index.php?requ=evenement-supprimer&id='.$_GET['id'].'">Supprimer l\'événement</a>';
+		?>
+		<h1>Détails de l'évènement</h1>
 			<article>
 				<span class="typeEvent type-<?php echo $event->getTypeEvenement()->getId();?>"><?php echo $event->getTypeEvenement()->getLibelle();?></span>
 				<h3><?php echo (($event->getDate())?(strftime('%A %d %B %Y', strtotime($event->getDate()))):'Pas de date annoncée');?></h3>
 				<h4>Commentaire</h4>
 				<p><?php echo $event->getCommentaire();?></p>
 			</article>
-			<?php if ($_SESSION['user_auth']['write'])
-				echo '<a href="evenement-editer/'.$_GET['id'].'">Editer l\'événement</a> <a href="index.php?requ=evenement-supprimer&id='.$_GET['id'].'">Supprimer l\'événement</a>';
-	} else {?>
-		<p class="warning">Cet événement n'existe pas</p>
-	<?php }?>
+<?php
+} else {?>
+		<p class="warning">Cet évènement n'existe pas</p>
+<?php }?>
 </div>
