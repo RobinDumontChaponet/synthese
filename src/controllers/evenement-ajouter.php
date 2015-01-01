b33c97941308e5a5870995601c62c2fabfb9b282
JozwickiVictor <tigre.demon@hotmail.fr>
2015-01-01 19:46:43 +0100

    Erreur + enregistrement auto si prefere

--- src/controllers/evenement-ajouter.php
+++ src/controllers/evenement-ajouter.php
@@ -1,6 +1,7 @@
 <?php
 if ($_SESSION['user_auth']['write']) {
 	require_once(MODELS_INC."EvenementDAO.class.php");
+	require_once(MODELS_INC."AParticipeDAO.class.php");
 
 	$typesEvent = TypeEvenementDAO::getAll();
 
@@ -9,7 +10,12 @@ if ($_SESSION['user_auth']['write']) {
 			$_POST['date'] = NULL;
 		$typeEvent = new TypeEvenement($_POST['typeEvent'], NULL);
 		$event = new Evenement(0, $typeEvent, $_POST['date'], $_POST['commentaire']);
-		EvenementDAO::create($event);
+		$idEvent = EvenementDAO::create($event);
+		$preferences = PrefereDAO::getByIdTypeEvent($_POST['typeEvent']);
+		foreach ($preferences as $preference) {
+			$participe = new AParticipe($preference->getAncien(), $event);
+			AParticipeDAO::create($participe);
+		}
 		header('Location: '.SELF.'evenements');
 	}
 	include(VIEWS_INC.'evenement-ajouter.php');
