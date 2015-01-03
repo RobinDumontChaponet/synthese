4ca03ae7640ae760b12bfaf5a7092aa23bfca07b
JozwickiVictor <tigre.demon@hotmail.fr>
2015-01-03 05:52:31 +0100

    A que c'est corrigé

--- src/controllers/promotion.php
+++ src/controllers/promotion.php
@@ -3,8 +3,11 @@
 if (isset($_GET['id'])) {
 	$promo = PromotionDAO::getById($_GET['id']);
 	$anciens = AncienDAO::getAncienByIdPromo($_GET['id']);
-} else
-	$anciens = null;
+} else {
+	$ancient = new Ancien($_SESSION["syntheseUser"]->getPersonne()->getId(), NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+	$aEtudie = AEtudieDAO::getByAncien($ancient);
+	header('Location: '.SELF.'promotion/'.$aEtudie->getPromotion()->getId());
+}
 
 include(VIEWS_INC.'promotion.php');
 
