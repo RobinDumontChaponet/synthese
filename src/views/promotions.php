7388ff43c1af7133760a77129022ef618cfc785f
JozwickiVictor <tigre.demon@hotmail.fr>
2015-01-15 11:53:28 +0100

    Ptit changement, robin tu pourras styler ?

--- src/views/promotions.php
+++ src/views/promotions.php
@@ -7,7 +7,7 @@
 	<section>
 		<ul>
 <?php foreach ($promotions as $promotion) {
-	echo '<li class="promotion"><a href="promotion/'.$promotion->getId().'" title="Promotion '.$promotion->getAnnee().'">Promotion '.$promotion->getAnnee().'</a> > ';
+	echo '<li class="promotion"><p>Promotion '.$promotion->getAnnee().'</p> ';
 	foreach ($departements as $departement)
 		echo '<a href="promotion/'.$promotion->getId().'/'.$departement->getSigle().'" title="Promotion '.$departement->getNom().' année '.$promotion->getAnnee().'">'.$departement->getSigle().'</a>';
 	echo '</li>'.PHP_EOL;
