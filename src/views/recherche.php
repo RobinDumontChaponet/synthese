89721a6861b7d9f73f5ca1c060fedc1614d99b23
fadiliyoussef <fadiliYouss@gmail.com>
2015-01-27 23:35:12 +0100

    Update recherche.php

--- src/views/recherche.php
+++ src/views/recherche.php
@@ -109,7 +109,7 @@
 	<nav class="pagination"></nav>
 
     <section id="actions">
-        <form method="POST" action="message">
+        <form method="POST" action="message-ecrire" id="send_message">
             <input type="submit" value="Envoyer un message" />
         </form>
         <?php if(isset($msgErrAdresse)){ echo $msgErrAdresse;} ?>
