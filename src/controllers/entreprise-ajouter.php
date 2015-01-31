551b074c19f7c59ada91da55cc84e45e73c9746e
JozwickiVictor <tigre.demon@hotmail.fr>
2015-01-31 16:03:33 +0100

    jiorjqozrljqzior

--- src/controllers/entreprise-ajouter.php
+++ src/controllers/entreprise-ajouter.php
@@ -1,7 +1,25 @@
 <?php
 
-$entreprise = EntrepriseDAO::getById($_GET['id']);
+$codesAPE = CodeAPEDAO::getAll();
 $libelleTypeProfil = $_SESSION['syntheseUser']->getTypeProfil()->getLibelle();
+
+if (isset($_POST) && $_POST != NULL) {
+	if ($_POST['name'] != NULL) {
+		var_dump($_POST['name']);
+		$valid = true;
+	}
+	if ($_POST['address1'] != NULL) {
+		var_dump($_POST['address1']);
+		$valid = true;
+	} else {
+
+	}
+
+	if ($valid) {
+		//$entreprise = new Entreprise(0, $_POST['name']);
+		//EntrepriseDAO::update($entreprise);
+	}
+}
 
 include(VIEWS_INC.'entreprise-ajouter.php');
 
--- src/views/entreprise-ajouter.php
+++ src/views/entreprise-ajouter.php
@@ -1,32 +1,38 @@
 <!--meta title="<?php echo ($entreprise != NULL)?$entreprise->getNom():'Entreprise non trouvé'; ?>" css="style/evenements.css"-->
 <div id="content">
-    <?php if ($_SESSION['user_auth']['write'])
-    echo '<a class="edit" href="entreprise-editer/'.$_GET['id'].'">Editer...</a>';
-    ?>
-    <h1>Détails de l'entreprise</h1>
+    <h1>Ajout d'une entreprise</h1>
+    <form action="entreprise-ajouter" method="post">
     <article>
-        <h3 class="entreprise"><?php echo $entreprise->getNom(); ?></h3>
         <dl>
-            <dt>Adresse 1</dt>
-            <dd id="adresse1"><?php echo $entreprise->getAdresse1(); ?></dd>
-            <dt>Adresse 2</dt>
-            <dd id="adresse2"><?php echo $entreprise->getAdresse2(); ?></dd>
-            <dt>Code postal</dt>
-            <dd id="codePostal"><?php echo $entreprise->getCodePostal(); ?></dd>
-            <dt>Ville</dt>
-            <dd id="ville"><?php echo $entreprise->getVille(); ?></dd>
-            <dt>Pays</dt>
-            <dd id="pays"><?php echo $entreprise->getPays(); ?></dd>
-            <dt>Code APE</dt>
-            <dd id="codeApe">
-            <?php
-            if($entreprise->getCodeAPE() == NULL) {
-                echo "Aucune code APE n'est associé à cette entreprise";
-            } else {
-                echo $entreprise->getCodeAPE()->getCode().' : '.$entreprise->getCodeAPE()->getLibelle();
-            }
-            ?>
+            <dt><label for="name">Nom de l'entreprise</label></dt>
+            <dd><input id="name" name="name"/></dd>
+
+            <dt><label for="address1">Adresse 1</label></dt>
+            <dd><input id="address1" name="address1"/></dd>
+
+            <dt><label for="address2">Adresse 2</label></dt>
+            <dd><input id="address2" name="address2"/></dd>
+
+            <dt><label for="postalCode">Code postal</label></dt>
+            <dd><input id="postalCode" name="postalCode"/></dd>
+
+            <dt><label for="city">Ville</label></dt>
+            <dd><input id="city" name="city"></dd>
+
+            <dt><label for="country">Pays</label></dt>
+            <dd><input id="country" name="country"/></dd>
+
+            <dt><label for="APEcode">Code APE</label></dt>
+            <dd>
+                <select name="APEcode">
+                <?php foreach($codesAPE as $codeAPE)
+                        echo '<option value="'.$codeAPE->getCode().'">'.$codeAPE->getCode().' - '.$codeAPE->getLibelle().'</option>';
+                    ?>
+                </select>
+                <a href="codesAPE">Accéder aux codes APE</a>
             </dd>
         </dl>
+        <input type="submit" value="Ajouter l'entreprise" />
     </article>
+    </form>
 </div>
