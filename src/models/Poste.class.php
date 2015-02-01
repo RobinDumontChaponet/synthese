ab17d713e3672ba6dfdf76d3c80f86ef0e6e133a
JozwickiVictor <tigre.demon@hotmail.fr>
2015-02-01 21:41:52 +0100

    Encore un petit truc

--- src/models/Poste.class.php
+++ src/models/Poste.class.php
@@ -1,65 +1,32 @@
 <?php
-class Poste
-{
-  private $id;
-  private $libelle;
-  
-  public function __construct($id, $libelle)
-  {
-      $this->setId($id);
-      $this->setLibelle($libelle);
-  }
-  
-//--------------------------------------------Getters
-  public function getId()
-  {
-      return $this->id;
-  }
-  
-  public function getLibelle()
-  {
-      return $this->libelle;
-  }
-//--------------------------------------------Setters
-  public function setId($id)
-  {
-    if(($id != null) and ($id >= 0))
-    {
-        $this->id = $id;
-    }else
-    {
-        throw new Exception("Id poste incorrect");
-    }
-  }
-  public function setLibelle($libelle)
-  {
-    $libelleTraite = trim($libelle);
-    if(($libelleTraite != null) and ($libelleTraite != ""))
-    {
-        $this->libelle = $libelleTraite;
-    }else
-    {
-        throw new Exception("Libellé poste incorrect");
-    }
-  }
-//-------------------------------------------toString
-  public function __toString()
-  {
-    return "Id : ".$this->id." Libellé : ".$this->libelle;
-  }
+class Poste {
+	
+	private $id;
+	private $libelle;
 
-//---------------------------------------------Equals
-	public function equals($aComparer)
-	{
-		if(get_class($aComparer) == "Poste")
-		{
-			return $this->id == $aComparer->getId();	
-		}else
-		{
-			return false;
-		}
+	public function __construct($id, $libelle) {
+		$this->setId($id);
+		$this->setLibelle($libelle);
 	}
-  
-  
-}
-?>
+
+//--------------------------------------------Getters
+	public function getId() {
+		return $this->id;
+	}
+
+	public function getLibelle() {
+		return $this->libelle;
+	}
+//--------------------------------------------Setters
+	public function setId($id) {
+		$this->id = $id;
+	}
+
+	public function setLibelle($libelle) {
+		$this->libelle = trim($libelle);
+	}
+//-------------------------------------------toString
+	public function __toString() {
+		return "Id : ".$this->id." Libellé : ".$this->libelle;
+	}
+} ?>
--- src/views/poste-ajouter.php
+++ src/views/poste-ajouter.php
@@ -1,10 +1,13 @@
 <!--meta title="Ajouter un poste" css="style/evenements.css"-->
 <div id="content">
 	<h1>Ajouter un poste</h1>
-	<article>
-		<dl>
-			<dt><label for="name">Nom</label></dt>
-			<dd><input id="name" name="name" type="text"></dd>
-		</dl>
-	</article>
+	<form action="poste-ajouter" method="post">
+		<article>
+			<dl>
+				<dt><label for="name">Nom</label></dt>
+				<dd><input id="name" name="name" type="text"></dd>
+			</dl>
+		</article>
+		<input type="submit" value="Ajouter le poste" />
+	</form>
 </div>
