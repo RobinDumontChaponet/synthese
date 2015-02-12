2b48fe577183cf25ac30f82191240bb9a3a6625b
fadiliyoussef <fadiliYouss@gmail.com>
2015-02-12 18:03:58 +0100

    Update message-ecrire.php
    Mise à jour messagerie

--- src/controllers/message-ecrire.php
+++ src/controllers/message-ecrire.php
@@ -2,39 +2,49 @@
 
 session_start();
 
+
 include_once(MODELS_INC.'AncienDAO.class.php');
 $anciens = array();
 
-if (isset($_POST['selectionne']) || isset($_POST['destinataire']) || isset($_POST['dest_rep'])) {
+if(isset($_POST['selectionne']) || isset($_POST['destinataire']) || isset($_POST['dest_rep']))
+{
 
-	if (isset($_POST['selectionne'])) {
-		//On recupere les adresses mail
-		foreach ($_POST['selectionne'] as $check) {
-			$ancien = AncienDAO::getById($check);
-			array_push($anciens, $ancien);
-			$chaine_mails .= $ancien->getPrenom().' '.$ancien->getNom().'; ';
-		}
-		$_SESSION['dest_anciens'] = $anciens;
-	} else if (isset($_POST['dest_rep'])) {
+if(isset($_POST['selectionne']))
+{
+    //On recupere les adresses mail
+    foreach($_POST['selectionne'] as $check)
+    {
+        $ancien = AncienDAO::getById($check);
+        array_push($anciens, $ancien);
+        $chaine_mails .= $ancien->getNomPatronymique().' '.$ancien->getPrenom().'; ';
+    }
+    $_SESSION['dest_anciens'] = $anciens;
+}else if(isset($_POST['dest_rep'])) {
 
-		$ancien = AncienDAO::getById($_POST['dest_rep']);
-		array_push($anciens, $ancien);
-		$_SESSION['dest_anciens'] = $anciens;
+    $ancien = AncienDAO::getById($_POST['dest_rep']);
+    array_push($anciens, $ancien);
+    $_SESSION['dest_anciens'] = $anciens;
 
-		$chaine_mails = $ancien->getMail();
-	}
+    $chaine_mails = $ancien->getNomPatronymique().' '.$ancien->getPrenom();
+}
 
-	if ( ($_POST['destinataire'] != '') && ($_POST['objet'] != '') && ($_POST['message'] != '') ) {
-		include_once('helpers/traitement_message.php'); //// BZZZZzzzz, WRONG !
-	} else {
-		$destinataire = ($chaine_mails == '')?$_POST['destinataire']:$chaine_mails;
-		$objet = $_POST['objet'];
-		$message = $_POST['message'];
-		include(VIEWS_INC.'message-ecrire.php');
-	}
-} else {
-	$msgErrAdresse = '<p class="warning">La sélection de destinataires est obligatoire !</p>';
-	include(VIEWS_INC.'recherche.php');
+
+    
+
+    if( ($_POST['destinataire'] != '') && ($_POST['objet'] != '') && ($_POST['message'] != '') )
+    {
+        include_once('helpers/traitement_message.php');
+    }else
+    {
+        $destinataire = ($chaine_mails == '')?$_POST['destinataire']:$chaine_mails;
+        $objet = $_POST['objet'];
+        $message = $_POST['message'];
+        include(VIEWS_INC.'message-ecrire.php');
+    }
+}else
+{
+    $msgErrAdresse = "Il faut obligatoirement sélectionner des étudiants !";
+    include(VIEWS_INC.'recherche.php');
 }
 
 ?>
