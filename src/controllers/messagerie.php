e278fba12feed8786863ae7064949b2bc447d8de
fadiliyoussef <fadiliYouss@gmail.com>
2015-02-12 18:10:59 +0100

    Mise à jour messagerie

--- src/controllers/messagerie.php
+++ src/controllers/messagerie.php
@@ -7,14 +7,14 @@ $nm = MessageDAO::getMessagesNonLus($_SESSION['syntheseUser']->getPersonne()->ge
 
 //La boucle for permet d'afficher du plus recent au moins recent, un foreach ne le fait pas
 for($i = (count($nm)-1); $i >= 0; $i--)
-    $nouveauxMessages .= '<tr><td>'.$nm[$i]->getExpediteur()->getNom().' '.$nm[$i]->getExpediteur()->getPrenom.' </td>
+    $nouveauxMessages .= '<tr><td>'.$nm[$i]->getExpediteur()->getNomPatronymique().' '.$nm[$i]->getExpediteur()->getPrenom.' </td>
     <td><a href="message/'.$nm[$i]->getId().'">'.$nm[$i]->getObjet().'</a> </td> <td>'.$nm[$i]->getDateMessage().'</td></tr></li>';
 
 
 //Traitement des messages non lus
 $messagesLus = MessageDAO::getMessagesLus($_SESSION['syntheseUser']->getPersonne()->getId());
 for($i = (count($messagesLus)-1); $i >= 0; $i--)
-    $anciensMessages .= '<tr><td>'.$messagesLus[$i]->getExpediteur()->getNom().' '.$messagesLus[$i]->getExpediteur()->getPrenom.' </td>
+    $anciensMessages .= '<tr><td>'.$messagesLus[$i]->getExpediteur()->getNomPatronymique().' '.$messagesLus[$i]->getExpediteur()->getPrenom.' </td>
     <td><a href="message/'.$messagesLus[$i]->getId().'">'.$messagesLus[$i]->getObjet().'</a> </td> <td>'.$messagesLus[$i]->getDateMessage().'</td></tr></li>';
 
 require_once(VIEWS_INC."messagerie.php");
