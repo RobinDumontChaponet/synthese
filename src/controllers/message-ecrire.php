44f0b11cd4fdb5d45f792ad18cba36da666afca8
fadiliyoussef <fadiliYouss@gmail.com>
2015-03-26 11:57:39 +0100

    MaJ message-ecrire

--- src/controllers/message-ecrire.php
+++ src/controllers/message-ecrire.php
@@ -10,30 +10,44 @@ $anciens = array();
 if(isset($_POST['selectionne']) || isset($_POST['destinataire']) || isset($_POST['dest_rep']) || isset($_GET['destinataire']))
 {
 
-if(isset($_POST['selectionne']))
-{
-    //On recupere les adresses mail
-    foreach($_POST['selectionne'] as $check)
+    if(isset($_POST['selectionne']))
     {
-        $ancien = AncienDAO::getById($check);
+        //On recupere les adresses mail
+       /* foreach($_POST['selectionne'] as $check)
+        {
+            $ancien = AncienDAO::getById($check);
+            array_push($anciens, $ancien);
+            $chaine_mails .= $ancien->getNomPatronymique().' '.$ancien->getPrenom().'; ';
+        }*/
+        
+        $anciens = getFromSelection();
+        foreach($anciens as $ancien)
+            $chaine_mails .= $ancien->getNomPatronymique().' '.$ancien->getPrenom().'; ';
+        
+        
+        $_SESSION['dest_anciens'] = $anciens;
+    }else if(isset($_POST['dest_rep'])) {
+        
+        $ancien = AncienDAO::getById($_POST['dest_rep']);
         array_push($anciens, $ancien);
-        $chaine_mails .= $ancien->getNomPatronymique().' '.$ancien->getPrenom().'; ';
+        $_SESSION['dest_anciens'] = $anciens;
+
+        $chaine_mails = $ancien->getNomPatronymique().' '.$ancien->getPrenom();
     }
-    $_SESSION['dest_anciens'] = $anciens;
-}else if(isset($_POST['dest_rep'])) {
-
-    $ancien = AncienDAO::getById($_POST['dest_rep']);
-    array_push($anciens, $ancien);
-    $_SESSION['dest_anciens'] = $anciens;
-
-    $chaine_mails = $ancien->getNomPatronymique().' '.$ancien->getPrenom();
-}
 
 
-    
 
     if( (trim($_POST['destinataire']) != '') && (trim($_POST['objet']) != '') && (trim($_POST['message']) != '') )
     {
+        
+        if (($_POST['chosen'] != '') && (!is_array($_SESSION['dest_anciens']))) {
+            $_SESSION['dest_anciens'] = completeFromInput($_POST['chosen']);
+        } else {
+            $_SESSION['dest_anciens'] = array_merge($_SESSION['dest_anciens'], completeFromInput($_POST['chosen']));
+        }
+         
+        
+        
         include_once('helpers/traitement_message.php');
     }else
     {
@@ -46,6 +60,41 @@ if(isset($_POST['selectionne']))
 {
     $msgErrAdresse = "Il faut obligatoirement sélectionner des étudiants !";
     include(CONTROLLERS_INC.'recherche.php');
+}
+
+
+function completeFromInput($ids='') {
+        $listComplete = array();
+        if ($ids != '') {
+                $ids = explode("-", $_POST['chosen']);
+                array_pop($ids);
+                foreach ($ids as $id) {
+                    $ancien = AncienDAO::getById($id);
+                    array_push($listComplete, $ancien);
+                }
+        }
+    return $listComplete;
+}
+
+function getFromSelection() {
+    $listComplete = array();
+    
+    $ids = explode("-", $_POST['infosCheck']);
+    array_pop($ids);
+
+    if ($ids[0] == 1) {
+        $all = AncienDAO::getAll();
+        for ($i = 0, $l = count($all); $i < $l; $i++) {
+            if (!in_array($all[$i]->getId(), $ids))
+                array_push($listComplete, $all[$i]);
+        }
+    } else {
+        for ($i = 1, $l = count($ids); $i < $l; $i++) {
+            $ancien = AncienDAO::getById($ids[$i]);
+            array_push($listComplete, $ancien);
+        }   
+    }
+    return $listComplete;
 }
 
 ?>
